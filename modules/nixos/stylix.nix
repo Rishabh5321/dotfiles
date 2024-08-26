6a5af1a5650925e1e0f179c246f867413575566a
Rishabh5321 <rishabh98818@outlook.com>
2024-08-26 16:55:56 +0530

    disabled hyprland xdg portal because not using hyprland

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -55,13 +55,13 @@
     enable = true;
     wlr.enable = true;
     extraPortals = [
-      #pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal-gtk
       pkgs.xdg-desktop-portal
     ];
     configPackages = [
       pkgs.xdg-desktop-portal-gnome
       pkgs.xdg-desktop-portal-gtk
-      pkgs.xdg-desktop-portal-hyprland
+      #pkgs.xdg-desktop-portal-hyprland
       pkgs.xdg-desktop-portal
     ];
   };
