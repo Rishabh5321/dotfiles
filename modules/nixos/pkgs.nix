db0cf4c0b443c725c88550776a069fc9b386bde0
Rishabh5321 <Rishabh5321@users.noreply.github.com>
2024-08-28 07:28:06 +0000

    chore: auto lint and format

--- modules/nixos/pkgs.nix
+++ modules/nixos/pkgs.nix
@@ -1,11 +1,11 @@
-{
-  pkgs,
-  inputs,
-  ...
+{ pkgs
+, inputs
+, ...
 }: {
-  environment.systemPackages = let
-    cursor = pkgs.callPackage ../../pkgs/cursor.nix {};
-  in
+  environment.systemPackages =
+    let
+      cursor = pkgs.callPackage ../../pkgs/cursor.nix { };
+    in
     with pkgs; [
       #age
       #amdvlk
@@ -177,7 +177,7 @@
       powerline
       nerdfonts
       material-icons
-      (nerdfonts.override {fonts = ["Meslo"];})
+      (nerdfonts.override { fonts = [ "Meslo" ]; })
     ];
   };
 }
