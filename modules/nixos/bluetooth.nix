b9231aa16e0ec325c82c1ccbe8294fba119fc317
Rishabh5321 <rishabh98818@outlook.com>
2024-08-30 09:30:32 +0530

    added some bluetooth settings

--- modules/nixos/bluetooth.nix
+++ modules/nixos/bluetooth.nix
@@ -3,4 +3,9 @@ _: {
   hardware.bluetooth.enable = true;
   #hardware.bluetooth.powerOnBoot = true;
   #services.blueman.enable = true;
+    hardware.bluetooth.settings = {
+    General = {
+      Enable = "Source,Sink,Media,Socket";
+    };
+  };
 }
