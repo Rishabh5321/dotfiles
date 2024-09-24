b7a229c9929e8a98b01938f2928b8cff36900912
Rishabh5321 <Rishabh5321@users.noreply.github.com>
2024-09-24 01:12:11 +0000

    chore: remove dead code

--- nixos/redmi/aagl.nix
+++ nixos/redmi/aagl.nix
@@ -1,4 +1,4 @@
-{ inputs, aagl, ... }: {
+{ aagl, ... }: {
   nix.settings = aagl.nixConfig; # Set up Cachix
   programs.anime-game-launcher.enable = true; # Adds launcher and /etc/hosts rules
   programs.anime-games-launcher.enable = true;
