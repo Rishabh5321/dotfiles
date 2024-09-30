bfc0e3421a9aceadea40e32dca5aa2aed91ca8ea
Rishabh5321 <rishabh98818@outlook.com>
2024-09-30 10:02:40 +0530

    removing theme name from spicetify beacause I stylix now probably supports spicetify natively

--- modules/home-manager/spicetify.nix
+++ modules/home-manager/spicetify.nix
@@ -17,7 +17,7 @@
         hidePodcasts
         shuffle # shuffle+ (special characters are sanitized out of extension names)
       ];
-      theme = spicePkgs.themes.ziro;
+      #theme = spicePkgs.themes.ziro;
       colorScheme = "custom";
       customColorScheme = {
         text = "${palette.base0B}";
