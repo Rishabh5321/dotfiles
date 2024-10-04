830bf5511403622f1e4816c8df29a090fa796a7b
Rishabh5321 <rishabh98818@outlook.com>
2024-10-04 09:29:13 +0530

    update disabled bliueman as gnome-bluetooth works

--- modules/nixos/bluetooth.nix
+++ modules/nixos/bluetooth.nix
@@ -2,7 +2,7 @@ _: {
   # Bluetooth Support
   hardware.bluetooth.enable = true;
   #hardware.bluetooth.powerOnBoot = true;
-  services.blueman.enable = true;
+  #services.blueman.enable = true;
   hardware.bluetooth.settings = {
     General = {
       Enable = "Source,Sink,Media,Socket";
