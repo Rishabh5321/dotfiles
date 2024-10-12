8241a8f86c3dfd50302e018d3d060e1344cb285d
Rishabh5321 <rishabh98818@outlook.com>
2024-10-12 14:19:53 +0530

    changed oh-my-posh theme

--- modules/home-manager/oh-my-posh.nix
+++ modules/home-manager/oh-my-posh.nix
@@ -1,7 +1,7 @@
 _: {
   programs.oh-my-posh = {
     enable = true;
-    useTheme = "paradox";
+    useTheme = "night-owl";
     enableBashIntegration = true;
     enableZshIntegration = true;
   };
