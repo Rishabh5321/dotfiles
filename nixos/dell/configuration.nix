51a3a050d685f279dfed41ff28711a260f38095f
Rishabh5321 <Rishabh5321@users.noreply.github.com>
2024-10-24 09:01:25 +0000

    chore: auto lint and format

--- nixos/dell/configuration.nix
+++ nixos/dell/configuration.nix
@@ -121,7 +121,7 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
 
-   xdg.portal = {
+  xdg.portal = {
     enable = true;
     wlr.enable = true;
     extraPortals = [
