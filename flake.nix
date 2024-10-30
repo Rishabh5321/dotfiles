2fe0cdaaa12f69cde256dad91842772473d87834
Rishabh5321 <rishabh98818@outlook.com>
2024-10-30 19:57:35 +0530

    changed wall

--- flake.nix
+++ flake.nix
@@ -67,7 +67,7 @@
       inherit (self) outputs;
       systems = [ "x86_64-linux" ];
       username = "rishabh";
-      wallpaper = "wall159.jpg";
+      wallpaper = "wall164.jpg";
       flakeDir = "~/dotfiles";
       pkgs-stable = import nixpkgs-stable {
         system = "x86_64-linux";
