bebdd5eed224292afd2d07283d15257952e2768f
Rishabh5321 <rishabh98818@outlook.com>
2024-11-18 16:17:29 +0530

    moved to 24.11 as 25.05 is unstable now

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     #nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     stylix.url = "github:danth/stylix";
