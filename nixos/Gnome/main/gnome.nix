11fc69611d86b5395769c02229dcd6593786c838
Rishabh5321 <rishabh98818@outlook.com>
2024-11-22 14:26:28 +0530

    Update for gnome settings

--- nixos/Gnome/main/gnome.nix
+++ nixos/Gnome/main/gnome.nix
@@ -9,52 +9,5 @@
   services = {
     # Browser integration
     gnome.gnome-browser-connector.enable = true;
-
-    # Trash folder in Gnome apps
-    # gvfs.enable = true;
-
-    udev.packages = with pkgs; [
-      gnome.gnome-settings-daemon
-    ];
   };
-
-  # System packages
-  environment.systemPackages = with pkgs;
-  with gnome;
-  with gnomeExtensions; [
-    # Essential
-    gnome-tweaks
-    gnome-settings-daemon
-
-    # Extensions
-    gsconnect # conflicts with kdeconnect, either one is fine or stop kdeconnect
-    clipman
-    user-avatar-in-quick-settings
-    wayland-or-x11
-    material-shell
-    appindicator
-    just-perfection
-    uptime-indicator
-    display-scale-switcher
-    privacy-settings-menu
-    blur-my-shell
-  ];
-
-  # Excluding unwanted packages
-  environment.gnome.excludePackages = with pkgs.gnome; [
-    gnome-system-monitor
-    gnome-text-editor
-    gnome-tour
-    gnome-connections
-    gnome-screenshot
-    cheese
-    epiphany
-    yelp
-    simple-scan
-    gnome-weather
-    gnome-contacts
-    gnome-clocks
-    gnome-maps
-    gnome-calculator
-  ];
 }
