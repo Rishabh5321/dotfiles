2e6ab4d86e9e1db9c6f3ea2b50f28c02d4832ad3
Rishabh5321 <rishabh98818@outlook.com>
2024-11-22 14:57:20 +0530

    sound update

--- modules/nixos/sound.nix
+++ modules/nixos/sound.nix
@@ -1,9 +1,19 @@
 _: {
   #sound.enable = true;
   hardware.pulseaudio.enable = false;
+
+  # make pipewire realtime-capable
+  security.rtkit.enable = true;
+
+  # pipewire low latency
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
     pulse.enable = true;
+    wireplumber.enable = true;
+
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
   };
 }
