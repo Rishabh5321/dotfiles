d2950819e08789e325f6c98d70ce9f668684a2af
Rishabh5321 <Rishabh5321@users.noreply.github.com>
2024-11-22 08:57:31 +0000

    chore: remove dead code

--- nixos/Gnome/main/gnome.nix
+++ nixos/Gnome/main/gnome.nix
@@ -1,6 +1,6 @@
 # Gnome Configuration
 #- <https://wiki.nixos.org/wiki/GNOME>
-{pkgs, ...}: {
+{ ...}: {
   services.xserver = {
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
