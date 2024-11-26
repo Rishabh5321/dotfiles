0ca16a353e24566eaee1beb915e6a4a6d41ab1ed
Rishabh5321 <rishabh98818@outlook.com>
2024-11-26 17:15:37 +0530

    update hyprland

--- modules/home-manager/hyprland/hyprland.nix
+++ modules/home-manager/hyprland/hyprland.nix
@@ -45,9 +45,10 @@ with lib; {
           exec-once = killall -q swaync;sleep .5 && swaync
           exec-once = nm-applet --indicator
           #exec-once = lxqt-policykit-agent
-          exec-once = hyprlock --immediate
+          #exec-once = hyprlock --immediate
           #exec-once = sleep 1.5 && startup
-          exec-once = sleep 5 && kdeconnect-settings && kdeconnect-indicator
+          exec-once = sleep 5 && tv.kodi.Kodi
+          #exec-once = sleep 5 && kdeconnect-settings && kdeconnect-indicator
           exec-once = wl-paste --type text --watch cliphist store #Stores only text data
           exec-once = wl-paste --type image --watch cliphist store #Stores only image data
           exec-once = sleep 1.5 && swww img /home/${username}/Pictures/Wallpapers/${wallpaper}
