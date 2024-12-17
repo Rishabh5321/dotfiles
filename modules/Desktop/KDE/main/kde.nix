cb9f22d84b9280705f2fbe2e162cefc02953cd2a
Rishabh5321 <rishabh98818@outlook.com>
2024-12-17 22:11:14 +0530

    Enable Plasma 6 desktop environment and SDDM display manager with Wayland support in Nix configuration for enhanced user experience and modern graphics capabilities.

--- modules/Desktop/KDE/main/kde.nix
+++ modules/Desktop/KDE/main/kde.nix
@@ -3,6 +3,14 @@
 _: {
   services = {
     desktopManager.plasma6.enable = true;
-    displayManager.sddm.enable = true;
+    displayManager = {
+      enable = true;
+      sddm = {
+        enable = true;
+          wayland = {
+          enable = true;
+        };
+      };
+    };
   };
 }
