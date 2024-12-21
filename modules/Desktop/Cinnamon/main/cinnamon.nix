25cc44cb392c4bf37acdc504f9726cbde830032a
Rishabh5321 <rishabh98818@outlook.com>
2024-12-21 16:26:46 +0530

    Update Nix configuration: enable LightDM display manager and Cinnamon desktop environment for enhanced user experience.

--- modules/Desktop/Cinnamon/main/cinnamon.nix
+++ modules/Desktop/Cinnamon/main/cinnamon.nix
@@ -1,5 +1,8 @@
 # Gnome Configuration
 #- <https://wiki.nixos.org/wiki/GNOME>
 _: {
-  
+	services.xserver = {
+		displayManager.lightdm.enable = true;
+		desktopManager.cinnamon.enable = true;
+	};  
 }
