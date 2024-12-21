8db5dfbbd8353277310e4dcf3efb4d858e35b70f
Rishabh5321 <40533251+Rishabh5321@users.noreply.github.com>
2024-12-21 14:07:31 +0000

    chore: auto lint and format

--- modules/Desktop/Cinnamon/main/cinnamon.nix
+++ modules/Desktop/Cinnamon/main/cinnamon.nix
@@ -1,8 +1,8 @@
 # Gnome Configuration
 #- <https://wiki.nixos.org/wiki/GNOME>
 _: {
-	services.xserver = {
-		displayManager.lightdm.enable = true;
-		desktopManager.cinnamon.enable = true;
-	};  
+  services.xserver = {
+    displayManager.lightdm.enable = true;
+    desktopManager.cinnamon.enable = true;
+  };
 }
