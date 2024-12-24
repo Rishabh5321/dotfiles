89875f5310ef03b2a147a75c9dd4979f54f578e3
Rishabh5321 <rishabh98818@outlook.com>
2024-12-24 12:41:14 +0530

    Update Nix configuration: enable Bluetooth support and services, including settings for auto-enable and controller mode, to enhance Bluetooth functionality.

--- modules/nixos/bluetooth.nix
+++ modules/nixos/bluetooth.nix
@@ -1,11 +1,25 @@
-_: {
-  # Bluetooth Support
-  hardware.bluetooth.enable = true;
-  #hardware.bluetooth.powerOnBoot = true;
-  #services.blueman.enable = true;
-  hardware.bluetooth.settings = {
-    General = {
-      Enable = "Source,Sink,Media,Socket";
+#
+#  Bluetooth
+#
+
+{ pkgs, ... }:
+
+{
+  hardware.bluetooth = {
+    enable = true;
+    settings = {
+      General = {
+        Enable = "Source,Sink,Media,Socket";
+        AutoEnable = true;
+        ControllerMode = "bredr";
+      };
     };
+  };
+  services.blueman.enable = true;
+  systemd.user.services.mpris-proxy = {
+    description = "Mpris proxy";
+    after = [ "network.target" "sound.target" ];
+    wantedBy = [ "default.target" ];
+    serviceConfig.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
   };
 }
