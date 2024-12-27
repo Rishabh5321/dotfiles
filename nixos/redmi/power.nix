b702a60ecb707f9799ed6d5e6d4d3fe0efd25b52
Rishabh5321 <rishabh98818@outlook.com>
2024-12-27 14:53:38 +0530

    Update NixOS power management settings: enable System76 scheduler and thermald service, while disabling GNOME's power management for improved performance on Intel CPUs.

--- nixos/redmi/power.nix
+++ nixos/redmi/power.nix
@@ -1,6 +1,6 @@
 _: {
   # Better scheduling for CPU cycles - thanks System76!!!
-  # services.system76-scheduler.settings.cfsProfiles.enable = true;
+  services.system76-scheduler.settings.cfsProfiles.enable = true;
 
   # Disable GNOMEs power management
   services.power-profiles-daemon.enable = false;
@@ -10,9 +10,5 @@ _: {
 
   # Enable thermald (only necessary if on Intel CPUs)
   services.thermald.enable = true;
-
-  #services.logind.lidSwitch = "suspend";
-
-  #services.logind.lidSwitchDocked = "ignore";
 
 }
