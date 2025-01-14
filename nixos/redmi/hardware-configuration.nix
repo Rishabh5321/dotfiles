573abe2d175c2296adad1cf118d4b0a53fa9a55e
Rishabh5321 <rishabh98818@outlook.com>
2025-01-14 11:24:01 +0530

    Update hardware configuration in NixOS: change UUIDs for root, home, and swap file systems, and adjust filesystem types from btrfs to ext4 for improved compatibility.

--- nixos/redmi/hardware-configuration.nix
+++ nixos/redmi/hardware-configuration.nix
@@ -10,26 +10,26 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" "btrfs" "i915" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" "i915" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/0068f31f-f31c-42e9-ab73-6b724583ff54";
-    fsType = "btrfs";
-    options = [ "subvol=@" ];
+    device = "/dev/disk/by-uuid/6788e7f9-4bdf-4c8b-8051-0206288f107e";
+    fsType = "ext4";
+  };
+
+  fileSystems."/home" = {
+    device = "/dev/disk/by-uuid/1ec11169-b3c3-4065-b9c9-e5aed1a75f3f";
+    fsType = "ext4";
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/6736-76F3";
+    device = "/dev/disk/by-uuid/3C2B-22B8";
     fsType = "vfat";
     options = [ "fmask=0022" "dmask=0022" ];
   };
-
-  swapDevices = [{
-    device = "/dev/disk/by-uuid/4ade3532-e7a7-4228-b546-88a66465feae";
-  }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
