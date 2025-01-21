6a306e2cf297adfb2c32a5da12859a43b771b356
Rishabh5321 <rishabh98818@outlook.com>
2025-01-21 10:46:00 +0530

    No changes made to pkgs/grayjay.nix; maintaining current state.

--- pkgs/grayjay.nix
+++ pkgs/grayjay.nix
@@ -1,74 +1,122 @@
-{ pkgs ? import <nixpkgs> { }
-,
+{
+  buildDotnetModule,
+  fetchgit,
+  dotnetCorePackages,
+  buildNpmPackage,
+  lib,
+  libz,
+  icu,
+  openssl,
+  xorg,
+  gtk3,
+  glib,
+  nss,
+  nspr,
+  dbus,
+  atk,
+  cups,
+  libdrm,
+  expat,
+  libxkbcommon,
+  pango,
+  cairo,
+  udev,
+  alsa-lib,
+  mesa,
+  libGL,
+  libsecret,
+  gitUpdater,
 }:
-(pkgs.buildFHSEnv rec {
-  pname = "grayjay";
-  version = "2";
-  # Absolute path to where the contents of Grayjay.Desktop-linux-x64-v{x} is located
-  installDir = "/home/rishabh/Grayjay";
-
-  # grayjay script wrapper that sets cwd to `installdir'
-  start-grayjay = pkgs.writeShellScriptBin "start-grayjay" ''cd ${installDir} && ./Grayjay'';
-
-  # grayjay desktop file.
-  grayjay-desktop-file = pkgs.makeDesktopItem {
-    name = "Grayjay";
-    type = "Application";
-    desktopName = "Grayjay";
-    genericName = "Desktop Client for Grayjay";
-    comment = "A desktop client for Grayjay to stream and download video content";
-    icon = "${installDir}/grayjay.png";
-    exec = pname;
-    path = installDir;
-    terminal = false;
-    categories = [ "Network" ];
-    keywords = [
-      "YouTube"
-      "Player"
-    ];
-    startupNotify = true;
-    startupWMClass = "Grayjay";
-    prefersNonDefaultGPU = false;
+let
+  version = "3";
+  src = fetchgit {
+    url = "https://gitlab.futo.org/videostreaming/Grayjay.Desktop";
+    tag = version;
+    hash = "sha256-gI1M/rDTSH16eeknHJfLA2iawoA5iGTKHfwUW4/lvv8=";
+    fetchSubmodules = true;
+    fetchLFS = true;
   };
-
-  targetPkgs =
-    _: with pkgs; [
-      start-grayjay
-      grayjay-desktop-file
-      libz
-      icu
-      openssl # For updater
-
-      xorg.libX11
-      xorg.libXcomposite
-      xorg.libXdamage
-      xorg.libXext
-      xorg.libXfixes
-      xorg.libXrandr
-      xorg.libxcb
-
-      gtk3
-      glib
-      nss
-      nspr
-      dbus
-      atk
-      cups
-      libdrm
-      expat
-      libxkbcommon
-      pango
-      cairo
-      udev
-      alsa-lib
-      mesa
-      libGL
-      libsecret
-
-    ];
-  runScript = "start-grayjay";
-  extraInstallCommands = ''
-    mkdir -p $out/share/applications/
-    cp ${grayjay-desktop-file}/share/applications/Grayjay.desktop $out/share/applications/
+  frontend = buildNpmPackage {
+    name = "grayjay-frontend";
+    inherit version src;
+    sourceRoot = "Grayjay.Desktop/Grayjay.Desktop.Web";
+    npmBuildScript = "build";
+    npmDepsHash = "sha256-pTEbMSAJwTY6ZRriPWfBFnRHSYufSsD0d+hWGz35xFM=";
+    installPhase = ''
+      runHook preInstall
+      cp -r dist/ $out
+      runHook postInstall
+    '';
+  };
+in
+buildDotnetModule {
+  pname = "grayjay";
+  inherit version src;
+  projectFile = [
+    "Grayjay.ClientServer/Grayjay.ClientServer.csproj"
+    "Grayjay.Engine/Grayjay.Engine/Grayjay.Engine.csproj"
+    "Grayjay.Desktop.CEF/Grayjay.Desktop.CEF.csproj"
+    "FUTO.MDNS/FUTO.MDNS/FUTO.MDNS.csproj"
+    "JustCef/DotCef.csproj"
+  ];
+  testProjectFile = [
+    "Grayjay.Desktop.Tests/Grayjay.Desktop.Tests.csproj"
+    "Grayjay.Engine/Grayjay.Engine.Tests/Grayjay.Engine.Tests.csproj"
+  ];
+  nugetDeps = ./grayjay_deps.json;
+  dotnet-runtime = dotnetCorePackages.aspnetcore_8_0;
+  executables = [ "Grayjay" ];
+  preBuild = ''
+    rm -r Grayjay.ClientServer/wwwroot/web
+    cp -r ${frontend} Grayjay.ClientServer/wwwroot/web
   '';
-})
+  postInstall = ''
+    chmod +x $out/lib/grayjay/cef/dotcefnative
+    rm $out/lib/grayjay/Portable
+    ln -s /tmp/grayjay-launch $out/lib/grayjay/launch
+    ln -s /tmp/grayjay-cef-launch $out/lib/grayjay/cef/launch
+  '';
+  makeWrapperArgs = [
+    "--chdir"
+    "${placeholder "out"}/lib/grayjay"
+  ];
+  runtimeDeps = [
+    libz
+    icu
+    openssl # For updater
+    xorg.libX11
+    xorg.libXcomposite
+    xorg.libXdamage
+    xorg.libXext
+    xorg.libXfixes
+    xorg.libXrandr
+    xorg.libxcb
+    gtk3
+    glib
+    nss
+    nspr
+    dbus
+    atk
+    cups
+    libdrm
+    expat
+    libxkbcommon
+    pango
+    cairo
+    udev
+    alsa-lib
+    mesa
+    libGL
+    libsecret
+  ];
+  passthru.updateScript = gitUpdater { };
+  meta = {
+    description = "Multi-platform media application that allows you to watch content from multiple platforms in a single application";
+    longDescription = ''Grayjay is a multi-platform media application that allows you to watch content from multiple platforms in a single application. Using an extendable plugin system developers can make new integrations with additional platforms. Plugins are cross-compatible between Android and Desktop.'';
+    homepage = "https://grayjay.app/desktop/";
+    # license = lib.licenses.sfl;
+    maintainers = with lib.maintainers; [ samfundev ];
+    platforms = lib.platforms.linux;
+    mainProgram = "Grayjay";
+  };
+}
