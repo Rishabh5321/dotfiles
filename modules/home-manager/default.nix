ea6d68ec3d1ca03a51eee0559542a4a9c0a7535a
Rishabh5321 <40533251+Rishabh5321@users.noreply.github.com>
2025-01-30 11:32:08 +0000

    chore: auto lint and format

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -26,8 +26,8 @@
   ];
   dconf.settings = {
     "org/virt-manager/virt-manager/connections" = {
-      autoconnect = ["qemu:///system"];
-      uris = ["qemu:///system"];
+      autoconnect = [ "qemu:///system" ];
+      uris = [ "qemu:///system" ];
     };
   };
 
