ed87e05afd5c098935e4bfff8159c379a064b35f
Rishabh5321 <40533251+Rishabh5321@users.noreply.github.com>
2025-02-08 15:34:21 +0000

    chore: auto lint and format

--- pkgs/migu.nix
+++ pkgs/migu.nix
@@ -1,4 +1,4 @@
-{pkgs}:
+{ pkgs }:
 pkgs.appimageTools.wrapType2 rec {
   name = "migu";
   pname = "migu";
@@ -9,21 +9,23 @@ pkgs.appimageTools.wrapType2 rec {
     hash = "sha256-eZEXrxBPvfteoBCKDGBlAFzQob9Ya11cbRqsbwgUPIw=";
   };
 
-  extraInstallCommands = let
-    contents = pkgs.appimageTools.extract {inherit pname version src;};
-  in ''
-    install -m 444 -D ${contents}/migu.desktop -t $out/share/applications
-    substituteInPlace $out/share/applications/migu.desktop --replace 'Exec=AppRun' 'Exec=${pname}'
-    cp -r ${contents}/usr/share/icons $out/share
-  '';
+  extraInstallCommands =
+    let
+      contents = pkgs.appimageTools.extract { inherit pname version src; };
+    in
+    ''
+      install -m 444 -D ${contents}/migu.desktop -t $out/share/applications
+      substituteInPlace $out/share/applications/migu.desktop --replace 'Exec=AppRun' 'Exec=${pname}'
+      cp -r ${contents}/usr/share/icons $out/share
+    '';
 
   meta = with pkgs.lib; {
     name = "migu";
     description = "Bittorrent streaming software for cats.";
     homepage = "https://github.com/ThaUnknown/migu";
     license = licenses.gpl3;
-    maintainers = with maintainers; [karitham];
-    platforms = ["x86_64-linux"];
+    maintainers = with maintainers; [ karitham ];
+    platforms = [ "x86_64-linux" ];
     mainProgram = "migu";
   };
 }
