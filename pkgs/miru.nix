fe9c4cca8571e0cc171f0daaed27a05332778ac3
Rishabh5321 <rishabh98818@outlook.com>
2025-02-10 09:59:21 +0530

    refactor(pkgs): Migrate Miru package to new AppImage packaging

--- pkgs/miru.nix
+++ pkgs/miru.nix
@@ -1,46 +1,29 @@
-{ pkgs }:
-
-with pkgs;
-
-stdenv.mkDerivation rec {
+{pkgs}:
+pkgs.appimageTools.wrapType2 rec {
+  name = "miru";
   pname = "miru";
-  version = "4.5.10";
+  version = "5.5.9";
 
-  src = fetchurl {
+  src = pkgs.fetchurl {
     url = "https://github.com/ThaUnknown/miru/releases/download/v${version}/linux-Miru-${version}.AppImage";
-    sha256 = "sha256-ptaviLwr0X/MuF517YLW7i9+rtnktcpgHVqMHn+tXWg=";
-    name = "linux-${pname}-${version}.AppImage";
+    hash = "sha256-AhaGiZ/Vx9nJmIXrzZ1JMLqjWfQDyoKpzl55NT712Ro=";
   };
 
-  appimageContents = appimageTools.extractType2 {
-    name = "${pname}-${version}";
-    inherit src;
+  extraInstallCommands = let
+    contents = pkgs.appimageTools.extract {inherit pname version src;};
+  in ''
+    install -m 444 -D ${contents}/miru.desktop -t $out/share/applications
+    substituteInPlace $out/share/applications/miru.desktop --replace 'Exec=AppRun' 'Exec=${pname}'
+    cp -r ${contents}/usr/share/icons $out/share
+  '';
+
+  meta = with pkgs.lib; {
+    name = "miru";
+    description = "Bittorrent streaming software for cats.";
+    homepage = "https://github.com/ThaUnknown/miru";
+    license = licenses.gpl3;
+    maintainers = with maintainers; [karitham];
+    platforms = ["x86_64-linux"];
+    mainProgram = "miru";
   };
-
-  dontUnpack = true;
-  dontConfigure = true;
-  dontBuild = true;
-
-  nativeBuildInputs = [ makeWrapper ];
-
-  installPhase = ''
-    runHook preInstall
-
-    mkdir -p $out/bin $out/share/${pname} $out/share/applications
-
-    cp -a ${appimageContents}/{locales,resources} $out/share/${pname}
-    cp -a ${appimageContents}/miru.desktop $out/share/applications/${pname}.desktop
-    cp -a ${appimageContents}/usr/share/icons $out/share
-
-    substituteInPlace $out/share/applications/${pname}.desktop \
-      --replace 'Exec=AppRun' 'Exec=${pname}'
-
-    runHook postInstall
-  '';
-
-  postFixup = ''
-    makeWrapper ${electron}/bin/electron $out/bin/${pname} \
-      --add-flags $out/share/${pname}/resources/app.asar \
-      --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath [ stdenv.cc.cc ]}"
-  '';
 }
