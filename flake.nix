8f4e0baa5032c47ab6ac0346af86ae2831948fdd
Rishabh5321 <rishabh98818@outlook.com>
2025-02-12 20:32:53 +0530

    chore: Update wallpapers and default wallpaper

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
       inherit (self) outputs;
       systems = [ "x86_64-linux" ];
       username = "rishabh";
-      wallpaper = "wall150.png";
+      wallpaper = "wall1.png";
       flakeDir = "~/dotfiles";
       pkgs-stable = import nixpkgs-stable {
         system = "x86_64-linux";
