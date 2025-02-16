baf87d3f01a6776cb48cb558041480830391f2b8
Rishabh5321 <40533251+Rishabh5321@users.noreply.github.com>
2025-02-16 18:28:07 +0530

    chore(home-manager): Commented out git configuration

--- nixos/redmi/home.nix
+++ nixos/redmi/home.nix
@@ -72,7 +72,7 @@
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
-  programs.git.enable = true;
+  #programs.git.enable = true;
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
