109112b8fdce885bc1bbeccf33de31ee72ac76ae
Rishabh5321 <40533251+Rishabh5321@users.noreply.github.com>
2025-02-25 13:32:44 +0530

    feat(flake): Add miru-flake and update package configuration
    - Integrate miru-flake as a new input in flake.nix
    - Update nixos/pkgs.nix to use miru package from miru-flake
    - Comment out local miru package definition
    - Update flake.lock with new input revisions

--- flake.nix
+++ flake.nix
@@ -13,6 +13,7 @@
     alejandra.url = "github:kamadorueda/alejandra";
     alejandra.inputs.nixpkgs.follows = "nixpkgs";
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
+    miru-flake.url = "github:Rishabh5321/miru-flake"; # Replace with your actual repository
     #fine-cmdline = {
     #  url = "github:VonHeikemen/fine-cmdline.nvim";
     #  flake = false;
@@ -34,6 +35,7 @@
     , home-manager
     , spicetify-nix
     , alejandra
+    , miru-flake
       #, darkmatter-grub-theme
     , grub2-themes
     , nix-flatpak
--- modules/nixos/pkgs.nix
+++ modules/nixos/pkgs.nix
@@ -16,12 +16,13 @@
     let
       cursor = pkgs.callPackage ../../pkgs/cursor.nix { };
       grayjay = pkgs.callPackage ../../pkgs/grayjay.nix { };
-      miru = pkgs.callPackage ../../pkgs/miru.nix { };
+      #miru = pkgs.callPackage ../../pkgs/miru.nix { };
       #houdoku = pkgs.callPackage ../../pkgs/houdoku.nix { };
       grayjay_bye = pkgs.callPackage ../../pkgs/grayjay_bye.nix { };
       akuse = pkgs.callPackage ../../pkgs/akuse.nix { };
     in
     with pkgs; [
+      inputs.miru-flake.packages.${system}.miru
       resilio-sync
       act #test github workflow
       akuse
@@ -30,7 +31,7 @@
       papirus-icon-theme
       #tela-icon-theme
       kdePackages.qtstyleplugin-kvantum
-      miru
+      #miru
       grayjay
       grayjay_bye
       mission-center
