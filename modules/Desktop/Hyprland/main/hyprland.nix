9daaf712b2ea7ac89a017664ddfce997f4104c16
Rishabh5321 <40533251+Rishabh5321@users.noreply.github.com>
2025-02-28 08:50:42 +0000

    chore: auto lint and format

--- modules/Desktop/Hyprland/main/hyprland.nix
+++ modules/Desktop/Hyprland/main/hyprland.nix
@@ -3,8 +3,8 @@
 { pkgs, ... }: {
 
   nix.settings = {
-    substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    substituters = [ "https://hyprland.cachix.org" ];
+    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
   };
   security.pam.services.hyprlock = {
     enableGnomeKeyring = true;
