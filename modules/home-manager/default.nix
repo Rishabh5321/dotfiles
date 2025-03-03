db699bebed399a0329b49e79c514850ae6be6537
Rishabh5321 <40533251+Rishabh5321@users.noreply.github.com>
2025-03-03 05:33:50 +0000

    chore: auto lint and format

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,6 +1,6 @@
 # Add your reusable home-manager modules to this directory, on their own file (https://nixos.wiki/wiki/Module).
 # These should be stuff you would like to share with others, not your personal configurations.
-{inputs,...}:{
+{ inputs, ... }: {
   # List your module files here
   # my-module = import ./my-module.nix;
   imports = [
@@ -34,6 +34,6 @@
   };
 
 
-modules.home.services.seanime.enable = true;
+  modules.home.services.seanime.enable = true;
 
 }
