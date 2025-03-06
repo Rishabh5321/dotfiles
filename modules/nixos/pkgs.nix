50b49cbe80d0ce338e2e8c3b8640f54a3c73dc7e
Rishabh5321 <40533251+Rishabh5321@users.noreply.github.com>
2025-03-06 20:48:52 +0530

    chore(grayjay): Update package to grayjay-with-updater

--- modules/nixos/pkgs.nix
+++ modules/nixos/pkgs.nix
@@ -61,7 +61,7 @@
       htop
       #inputs.akuse-flake.packages.${system}.akuse
       inputs.alejandra.defaultPackage.${system}
-      inputs.grayjay.packages.${pkgs.system}.grayjay-with-desktop
+      inputs.grayjay.packages.${pkgs.system}.grayjay-with-updater
       inputs.zen-browser.packages."${system}".default
       jellyfin-media-player
       kdePackages.qtstyleplugin-kvantum
