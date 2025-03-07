34d797e44e53a3c49c564092436ca63546198542
Rishabh5321 <40533251+Rishabh5321@users.noreply.github.com>
2025-03-07 13:37:44 +0530

    feat(btop,fzf): Enhance system monitoring and fuzzy finder configs

--- modules/home-manager/btop.nix
+++ modules/home-manager/btop.nix
@@ -1,10 +1,14 @@
-_: {
-  programs = {
-    btop = {
-      enable = true;
-      settings = {
-        vim_keys = true;
-      };
+{ pkgs, ... }:
+{
+  programs.btop = {
+    enable = true;
+
+    settings = {
+      theme_background = false;
+      update_ms = 1000;
+      rounded_corners = false;
     };
   };
+
+  home.packages = (with pkgs; [ nvtopPackages.intel ]);
 }
--- modules/home-manager/fzf.nix
+++ modules/home-manager/fzf.nix
@@ -1,24 +1,28 @@
-_: {
+{ pkgs, ... }:
+{
   programs.fzf = {
     enable = true;
+
     enableZshIntegration = true;
-    # colors = {
-    #   "fg+" = config.stylix.base16Scheme.base00;
-    #   "bg+" = "-1";
-    #   "fg" = "white";
-    #   "bg" = "-1";
-    #   "prompt" = "grey";
-    #   "pointer" = config.stylix.base16Scheme.base01;
-    # };
+
+    defaultCommand = "fd --hidden --strip-cwd-prefix --exclude .git";
+    fileWidgetOptions = [
+      "--preview 'if [ -d {} ]; then eza --tree --color=always {} | head -200; else bat -n --color=always --line-range :500 {}; fi'"
+    ];
+    changeDirWidgetCommand = "fd --type=d --hidden --strip-cwd-prefix --exclude .git";
+    changeDirWidgetOptions = [
+      "--preview 'eza --tree --color=always {} | head -200'"
+    ];
+
+    ## Theme
     defaultOptions = [
-      "--margin=1"
-      "--layout=reverse"
-      "--border=rounded"
-      "--info='hidden'"
-      "--header=''"
-      "--prompt='/ '"
-      "-i"
-      "--no-bold"
+      "--color=fg:-1,fg+:#FBF1C7,bg:-1,bg+:#282828"
+      "--color=hl:#98971A,hl+:#B8BB26,info:#928374,marker:#D65D0E"
+      "--color=prompt:#CC241D,spinner:#689D6A,pointer:#D65D0E,header:#458588"
+      "--color=border:#665C54,label:#aeaeae,query:#FBF1C7"
+      "--border='double' --border-label='' --preview-window='border-sharp' --prompt='> '"
+      "--marker='>' --pointer='>' --separator='─' --scrollbar='│'"
+      "--info='right'"
     ];
   };
 }
