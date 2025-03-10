db1fa97ae26f1f451e198a05224ad45d6f49b063
Rishabh5321 <40533251+Rishabh5321@users.noreply.github.com>
2025-03-10 11:02:04 +0530

    feat(desktop): Add Cosmic desktop configuration and cachix

--- modules/Desktop/Cosmic/main/cosmic.nix
+++ modules/Desktop/Cosmic/main/cosmic.nix
@@ -1,6 +1,15 @@
-# Gnome Configuration
-#- <https://wiki.nixos.org/wiki/GNOME>
-_: {
+{ config, pkgs, inputs, ... }:
+ {
+
+  imports = [
+    inputs.nixos-cosmic.nixosModules.default
+  ];
+
+  nix.settings = {
+    substituters = [ "https://cosmic.cachix.org/" ];
+    trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+  };
+
   services = {
     displayManager.cosmic-greeter.enable = true;
     desktopManager.cosmic.enable = true;
