fd85679b327196c341eb48ea17756aabfe4dcb3a
Rishabh5321 <40533251+Rishabh5321@users.noreply.github.com>
2025-03-12 14:56:32 +0530

    feat(flatpak,pkgs): update flatpak packages and remove varia package

--- modules/nixos/flatpak.nix
+++ modules/nixos/flatpak.nix
@@ -3,23 +3,13 @@ _:
   services.flatpak = {
     enable = true;
     packages = [
-      "org.dupot.easyflatpak"
+      #"org.dupot.easyflatpak"
       "tv.kodi.Kodi"
       "org.jdownloader.JDownloader"
       #"com.github.iwalton3.jellyfin-media-player"
-      #"io.github.giantpinkrobots.varia"
-      "info.febvre.Komikku"
+      "io.github.giantpinkrobots.varia"
+      #"info.febvre.Komikku"
     ];
-    overrides = {
-      global = {
-        # Force Wayland by default
-        Context.sockets = [
-          "wayland"
-          "!x11"
-          "!fallback-x11"
-        ];
-      };
-    };
     update = {
       auto = {
         enable = true;
