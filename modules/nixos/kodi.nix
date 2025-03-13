13007520e8d66242d474ddb11d041d718d9899b0
Rishabh5321 <40533251+Rishabh5321@users.noreply.github.com>
2025-03-13 04:02:05 +0000

    chore: auto lint and format

--- modules/nixos/kodi.nix
+++ modules/nixos/kodi.nix
@@ -1,6 +1,6 @@
-{pkgs, ...}:{
+{ pkgs, ... }: {
   services.xserver.desktopManager.kodi = {
-      enable = true;
-      package = pkgs.kodi.withPackages (p: with p; [ inputstream-adaptive ]);
-    };
+    enable = true;
+    package = pkgs.kodi.withPackages (p: with p; [ inputstream-adaptive ]);
+  };
 }
