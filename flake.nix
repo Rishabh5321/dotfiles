1a20ceb8726c48b7456744f0ed166a7ecebf5f31
Rishabh5321 <40533251+Rishabh5321@users.noreply.github.com>
2025-03-18 20:30:53 +0530

    feat(iso): add custom NixOS ISO configuration and package management

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
   description = "Laptop and server config";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
@@ -33,7 +32,6 @@
     #cursor-flake.url = "github:Rishabh5321/cursor-flake";
     #grub2-themes.url = "github:vinceliuice/grub2-themes";
   };
-
   outputs = { self, nixpkgs, nixpkgs-stable, home-manager, spicetify-nix, akuse-flake, darkmatter-grub-theme, nix-flatpak, ... } @ inputs:
     let
       inherit (self) outputs;
@@ -50,7 +48,6 @@
         config = { allowUnfree = true; };
       };
       forAllSystems = nixpkgs.lib.genAttrs systems;
-
       commonConfig = { hostname }: {
         specialArgs = {
           inherit inputs outputs username home-manager wallpaper spicetify-nix flakeDir pkgs-stable;
@@ -81,12 +78,72 @@
       };
     in
     {
-      packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+      # Define all packages in one location
+      packages = forAllSystems (system: 
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          customPkgs = import ./pkgs pkgs;
+        in
+        customPkgs // {
+          # Add ISO image to packages
+          iso = self.nixosConfigurations.iso.config.system.build.isoImage;
+          # Set default package
+          default = customPkgs.default or self.nixosConfigurations.iso.config.system.build.isoImage;
+        }
+      );
+      
       #formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
       overlays = import ./overlays { inherit inputs; };
       nixosConfigurations = {
         redmi = nixpkgs.lib.nixosSystem (commonConfig { hostname = "redmi"; });
         dell = nixpkgs.lib.nixosSystem (commonConfig { hostname = "dell"; });
+        
+        # ISO configuration
+        iso = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { 
+            inherit inputs outputs username wallpaper spicetify-nix flakeDir pkgs-stable;
+          };
+          modules = [
+            # Base ISO configuration
+            "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+            # Our custom ISO configuration
+            ({ config, lib, pkgs, ... }: {
+              imports = [ ./nixos/iso/configuration.nix ];
+              
+              # Explicitly disable wireless to avoid conflict with NetworkManager
+              networking.wireless.enable = lib.mkForce false;
+              
+              # Basic ISO configuration
+              networking.hostName = "nixos-live";
+              users.users.${username} = {
+                isNormalUser = true;
+                extraGroups = [ "wheel" "networkmanager" "video" "audio" ];
+                initialPassword = "nixos";
+              };
+              
+              # Add some basic packages for installation
+              environment.systemPackages = with pkgs; [
+                git
+                wget
+                curl
+                vim
+                gparted
+                htop
+                inputs.akuse-flake.packages."x86_64-linux".akuse
+              ];
+              
+              # Make sure stylix has access to the wallpaper
+              stylix = {
+                enable = true;
+                image = ./nixos/wallpapers/${wallpaper};
+                base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+              };
+            })
+            # Add stylix module
+            inputs.stylix.nixosModules.stylix
+          ];
+        };
       };
     };
 }
---
+++ nixos/iso/configuration.nix
@@ -0,0 +1,116 @@
+# nixos/iso/configuration.nix
+{ config, lib, pkgs, inputs, username, ... }:
+
+{
+  # ISO-specific configurations
+  
+  # Enable networking - fix conflict between NetworkManager and wireless
+  networking = {
+    networkmanager.enable = true;
+    # Disable default wireless configuration since we're using NetworkManager
+    wireless.enable = false;
+  };
+  
+  # Enable SSH for remote assistance during installation
+  services.openssh.enable = true;
+  services.openssh.settings.PermitRootLogin = "yes";
+  
+  # Set up basic system configuration
+  time.timeZone = "Asia/Kolkata";  # Adjust to your timezone
+  i18n.defaultLocale = "en_US.UTF-8";
+  
+  # Configure console keymap
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
+  };
+  
+  # Enable basic graphical environment
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+  };
+  
+  # Add the flake to the ISO so it can be easily accessed
+  system.activationScripts = {
+    dotfiles = ''
+      mkdir -p /home/${username}/dotfiles
+      cp -r ${../..}/* /home/${username}/dotfiles/
+      chown -R ${username}:users /home/${username}/dotfiles
+    '';
+  };
+  
+  # Add helpful tools for installation
+  environment.systemPackages = with pkgs; [
+    firefox
+    gnome-terminal
+    nautilus
+    gnome-system-monitor
+    parted
+    gnome-disk-utility
+    gptfdisk
+    ntfs3g
+    cryptsetup
+    btrfs-progs
+    e2fsprogs
+    dosfstools
+    pciutils
+    usbutils
+    git
+    neofetch
+    vscode
+  ];
+  
+  # # Configure stylix with your wallpaper
+  # stylix = {
+  #   enable = true;
+  #   image = ../wallpapers/${config.wallpaper};
+  #   base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+  #   cursor = {
+  #     package = pkgs.bibata-cursors;
+  #     name = "Bibata-Modern-Classic";
+  #   };
+  # };
+  
+  # Enable nix flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.allowUnfree = true;
+  # Create installation helpers
+  environment.etc."install-system.sh" = {
+    text = ''
+      #!/bin/sh
+      echo "This script will help you install your NixOS system using your flake"
+      echo "Your flake is available at /home/${username}/dotfiles"
+      echo ""
+      echo "Quick commands:"
+      echo "- cd /home/${username}/dotfiles"
+      echo "- sudo nixos-install --flake .#redmi"
+      echo "- sudo nixos-install --flake .#dell"
+    '';
+    mode = "0755";
+  };
+  
+  # Create a simple readme
+  environment.etc."README.md" = {
+    text = ''
+      # NixOS Installation ISO
+      
+      This is a custom NixOS installation ISO with your configuration flake included.
+      
+      ## Installation Instructions
+      
+      1. Connect to the internet using NetworkManager (nm-connection-editor)
+      2. Partition your disks using GParted or the command line tools
+      3. Mount your partitions under /mnt
+      4. Run the installation:
+         ```
+         sudo nixos-install --flake /home/${username}/dotfiles#your-hostname
+         ```
+      5. Reboot into your new system
+      
+      Your flake is available at `/home/${username}/dotfiles`
+    '';
+    mode = "0644";
+  };
+}
