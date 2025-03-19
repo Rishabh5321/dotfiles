f947506f8712b6421e35c8af945eef5725668c30
Rishabh5321 <40533251+Rishabh5321@users.noreply.github.com>
2025-03-19 11:17:07 +0530

    Making error to check cache and workflow

--- nixos/dell/configuration.nix
+++ nixos/dell/configuration.nix
@@ -32,7 +32,7 @@
   #drivers.amdgpu.enable = false;
   #nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  networking.hostName = "dell"; # Define your hostname.
+  networking.hostName = "dell" # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
