1276f3d7e3ae085a608812035bdfaf77c10ebcae
Rishabh5321 <40533251+Rishabh5321@users.noreply.github.com>
2025-03-19 05:14:28 +0000

    chore: auto lint and format

--- nixos/dell/configuration.nix
+++ nixos/dell/configuration.nix
@@ -42,123 +42,123 @@
   # Enable networking
   networking.networkmanager.enable = true
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
+    # Enable the X11 windowing system.
+    # You can disable this if you're only using the Wayland session.
 
-  services = {
-    xserver = {
-      enable = true;
-      #displayManager.lightdm.enable = true;
-      #desktopManager.xfce = {
-      #  enable = true;
-      #enableXfwm = false;
-      #};
-      #displayManager.gdm.enable = true;
-      #desktopManager.gnome.enable = true;
-      xkb = {
-        layout = "us";
-        variant = "";
-      };
-      #windowManager.i3.enable = true;
-    };
-    #displayManager.defaultSession = "hyprland";
-  };
-
-  #services.displayManager = {
+    services = {
+  xserver = {
+  enable = true;
+  #displayManager.lightdm.enable = true;
+  #desktopManager.xfce = {
   #  enable = true;
-  #  sddm = {
-  #    enable = true;
-  #    wayland = {
-  #      enable = true;
-  #    };
-  #    settings = {
-  #      Autologin = {
-  #        Session = "hyprland";
-  #        User = "rishabh";
-  #      };
-  #    };
-  #    autoNumlock = true;
-  #  };
+  #enableXfwm = false;
   #};
-  #services.desktopManager.cosmic.enable = true;
-
-  #services.displayManager.sddm.theme = "sddm-astronaut-theme";
-  #services.desktopManager.plasma6.enable = true;
-  #services.displayManager.sddm.enable = true;
-
-  #services.xserver = {
-  #  enable = true;
-  #  desktopManager = {
-  #    xterm.enable = false;
-  #    xfce = {
-  #      enable = true;
-  #      #noDesktop = true;
-  #      enableXfwm = false;
-  #    };
-  #  };
-  #  xkb = {
-  #    layout = "us";
-  #    variant = "";
-  #  };
-  #  windowManager.i3.enable = true;
-  #};
-  #services.displayManager.defaultSession = "xfce";
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
-
-  services.samba = {
-    enable = true;
-    settings = {
-      "root" = {
-        path = "/"; # Replace with the directory you want to share
-        browseable = true; # Allow it to be seen in the network browser
-        readOnly = false; # Enable write access
-        validUsers = [ "rishabh" ]; # Use 'rishabh' as the username
-      };
-      "mnt" = {
-        path = "/mnt"; # Replace with the directory you want to share
-        browseable = true; # Allow it to be seen in the network browser
-        readOnly = false; # Enable write access
-        validUsers = [ "rishabh" ]; # Use 'rishabh' as the username
-      };
-      "home" = {
-        path = "/home/rishabh"; # Replace with the directory you want to share
-        browseable = true; # Allow it to be seen in the network browser
-        readOnly = false; # Enable write access
-        validUsers = [ "rishabh" ]; # Use 'rishabh' as the username
-      };
-    };
+  #displayManager.gdm.enable = true;
+  #desktopManager.gnome.enable = true;
+  xkb = {
+    layout = "us";
+    variant = "";
   };
+  #windowManager.i3.enable = true;
+};
+#displayManager.defaultSession = "hyprland";
+};
+
+#services.displayManager = {
+#  enable = true;
+#  sddm = {
+#    enable = true;
+#    wayland = {
+#      enable = true;
+#    };
+#    settings = {
+#      Autologin = {
+#        Session = "hyprland";
+#        User = "rishabh";
+#      };
+#    };
+#    autoNumlock = true;
+#  };
+#};
+#services.desktopManager.cosmic.enable = true;
+
+#services.displayManager.sddm.theme = "sddm-astronaut-theme";
+#services.desktopManager.plasma6.enable = true;
+#services.displayManager.sddm.enable = true;
+
+#services.xserver = {
+#  enable = true;
+#  desktopManager = {
+#    xterm.enable = false;
+#    xfce = {
+#      enable = true;
+#      #noDesktop = true;
+#      enableXfwm = false;
+#    };
+#  };
+#  xkb = {
+#    layout = "us";
+#    variant = "";
+#  };
+#  windowManager.i3.enable = true;
+#};
+#services.displayManager.defaultSession = "xfce";
+
+# Enable CUPS to print documents.
+services.printing.enable = true;
+
+# Enable touchpad support (enabled default in most desktopManager).
+# services.xserver.libinput.enable = true;
+
+# Some programs need SUID wrappers, can be configured further or are
+# started in user sessions.
+# programs.mtr.enable = true;
+# programs.gnupg.agent = {
+#   enable = true;
+#   enableSSHSupport = true;
+# };
+
+# List services that you want to enable:
+
+# Enable the OpenSSH daemon.
+# services.openssh.enable = true;
+
+# Open ports in the firewall.
+# networking.firewall.allowedTCPPorts = [ ... ];
+# networking.firewall.allowedUDPPorts = [ ... ];
+# Or disable the firewall altogether.
+networking.firewall.enable = false;
+
+# This value determines the NixOS release from which the default
+# settings for stateful data, like file locations and database versions
+# on your system were taken. It‘s perfectly fine and recommended to leave
+# this value at the release version of the first install of this system.
+# Before changing this value read the documentation for this option
+# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+system.stateVersion = "25.05"; # Did you read the comment?
+
+services.samba = {
+enable = true;
+settings = {
+"root" = {
+path = "/"; # Replace with the directory you want to share
+browseable = true; # Allow it to be seen in the network browser
+readOnly = false; # Enable write access
+validUsers = [ "rishabh" ]; # Use 'rishabh' as the username
+};
+"mnt" = {
+path = "/mnt"; # Replace with the directory you want to share
+browseable = true; # Allow it to be seen in the network browser
+readOnly = false; # Enable write access
+validUsers = [ "rishabh" ]; # Use 'rishabh' as the username
+};
+"home" = {
+path = "/home/rishabh"; # Replace with the directory you want to share
+browseable = true; # Allow it to be seen in the network browser
+readOnly = false; # Enable write access
+validUsers = [ "rishabh" ]; # Use 'rishabh' as the username
+};
+};
+};
 
 }
