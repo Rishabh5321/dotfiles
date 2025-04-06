5aae4a46ec1b2a0433a879914027061e1341ac96
Rishabh5321 <40533251+Rishabh5321@users.noreply.github.com>
2025-04-06 10:10:12 +0530

    feat(usbguard): enhance USBGuard configuration for user access and permissions

--- modules/nixos/usbguard.nix
+++ modules/nixos/usbguard.nix
@@ -1,8 +1,16 @@
-_: {
+{username,...}: {
   services.usbguard = {
     enable = true;
     presentControllerPolicy = "apply-policy";
-    IPCAllowedGroups = [ "wheel" ];
+    IPCAllowedGroups = [ "usbguard" ];
     dbus.enable = true;
   };
+  users.users.${username} = {
+    extraGroups = [ "usbguard" "plugdev" ];
+  };
+  users.groups.usbguard = { };
+  services.udev.extraRules = ''
+    SUBSYSTEM=="usb", MODE="0664", GROUP="plugdev"
+  '';
+  nix.settings.allowed-users = [ "@wheel" "${username}" ];
 }
