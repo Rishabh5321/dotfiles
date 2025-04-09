ff612e0cb297c46793402f6279c6650a013517ea
Rishabh <40533251+Rishabh5321@users.noreply.github.com>
2025-04-09 10:34:02 +0530

    chore: auto lint and format (#1568)

--- modules/nixos/cache.nix
+++ modules/nixos/cache.nix
@@ -28,6 +28,6 @@
       "nix-config.cachix.org-1:Vd6raEuldeIZpttVQfrUbLvXJHzzzkS0pezXCVVjDG4="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="
-      ];
+    ];
   };
 }
