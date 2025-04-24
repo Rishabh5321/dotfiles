13ecb36f8d859c91dae93eb08a077ea4509cc5b0
Rishabh5321 <40533251+Rishabh5321@users.noreply.github.com>
2025-04-24 13:37:20 +0530

    chore(hyprpanel): update layout configuration and enable additional modules

--- modules/desktop/Hyprland/home/hyprpanel.nix
+++ modules/desktop/Hyprland/home/hyprpanel.nix
@@ -10,12 +10,12 @@ in
 {
   imports = [
     inputs.hyprpanel.homeManagerModules.hyprpanel
-    inputs.ags.homeManagerModules.default
   ];
 
   programs.hyprpanel = {
-    overlay.enable = true;
+    # overlay.enable = false;
     enable = true;
+    # systemd.enable = true;
     hyprland.enable = true;
     overwrite.enable = true;
     settings.layout = {
@@ -26,16 +26,9 @@ in
             "workspaces"
             "windowtitle"
           ];
-          "middle" = [
-            "clock"
-            "media"
-          ];
+          "middle" = [ ]; # Add this line
           "right" = [
-            "volume"
-            "bluetooth"
-            "network"
-            "systray"
-            "notifications"
+            "clock"
           ];
         };
 
@@ -47,10 +40,27 @@ in
           ];
           "middle" = [
             "clock"
+            "media"
           ];
           "right" = [
             "volume"
-            "bluetooth"
+            "network"
+            "systray"
+            "notifications"
+          ];
+        };
+        "2" = {
+          "left" = [
+            "dashboard"
+            "workspaces"
+            "windowtitle"
+          ];
+          "middle" = [
+            "clock"
+            "media"
+          ];
+          "right" = [
+            "volume"
             "network"
             "systray"
             "notifications"
@@ -60,88 +70,88 @@ in
     };
 
     override = {
-      "tear" = true; # Screen Tearing
-      "theme.font.size" = "1.1rem";
-      "theme.bar.outer_spacing" = "1rem";
-      "theme.bar.dropdownGap" = "3.3em";
-      "scalingPriority" = "hyprland";
-      "bar.launcher.icon" = "";
-
-      "bar.workspaces.showAllActive" = false;
-      "bar.workspaces.workspaces" = 1;
-      "bar.workspaces.monitorSpecific" = false;
-      "bar.workspaces.hideUnoccupied" = true;
-      "bar.workspaces.showApplicationIcons" = true;
-      "bar.workspaces.showWsIcons" = true;
-      "bar.workspaces.ignored" = "98";
-
-      "bar.windowtitle.label" = false;
-      "bar.clock.format" = "%b %d  %k:%M";
-      "bar.clock.showIcon" = false;
-      "bar.volume.label" = true;
-      "bar.bluetooth.label" = false;
-      "bar.network.label" = false;
-      "bar.media.show_active_only" = true;
-      "menus.clock.weather.enable" = false;
-
-      "theme.bar.menus.monochrome" = true;
-      "theme.bar.buttons.monochrome" = true;
-      "wallpaper.enable" = false;
-
-      "theme.bar.buttons.workspaces.hover" = "${accent-alt}";
-      "theme.bar.buttons.workspaces.active" = "${accent}";
-      "theme.bar.buttons.workspaces.available" = "${accent-alt}";
-      "theme.bar.buttons.workspaces.occupied" = "${accent-alt}";
-      "theme.bar.menus.background" = "${background}";
-      "theme.bar.menus.cards" = "${background-alt}";
-      "theme.bar.menus.card_radius" = "${toString rounding}px";
-      "theme.bar.menus.label" = "${foreground}";
-      "theme.bar.menus.text" = "${foreground}";
-      "theme.bar.menus.border.color" = "${accent}";
-      "theme.bar.menus.border.radius" = "${toString rounding}px";
-      "theme.bar.menus.popover.text" = "${foreground}";
-      "theme.bar.menus.popover.background" = "${background-alt}";
-      "theme.bar.menus.listitems.active" = "${accent}";
-      "theme.bar.menus.icons.active" = "${accent}";
-      "theme.bar.menus.switch.enabled" = "${accent}";
-      "theme.bar.menus.check_radio_button.active" = "${accent}";
-      "theme.bar.menus.buttons.default" = "${accent}";
-      "theme.bar.menus.buttons.active" = "${accent}";
-      "theme.notification.border_radius" = "${toString rounding}px";
-      "theme.bar.menus.iconbuttons.active" = "${accent}";
-      "theme.bar.menus.progressbar.foreground" = "${accent}";
-      "theme.bar.menus.slider.primary" = "${accent}";
-      "theme.bar.menus.tooltip.background" = "${background-alt}";
-      "theme.bar.menus.tooltip.text" = "${foreground}";
-      "theme.bar.menus.dropdownmenu.background" = "${background-alt}";
-      "theme.bar.menus.dropdownmenu.text" = "${foreground}";
-      "theme.bar.background" = "${background}";
-      "theme.bar.buttons.text" = "${foreground}";
-      "theme.bar.buttons.radius" = "${toString rounding}px";
-      "theme.bar.buttons.background" = "${background-alt}";
-      "theme.bar.buttons.icon" = "${accent}";
-      "theme.bar.buttons.notifications.background" = "${background-alt}";
-      "theme.bar.buttons.hover" = "${background}";
-      "theme.bar.buttons.notifications.hover" = "${background}";
-      "theme.bar.buttons.notifications.total" = "${accent}";
-      "theme.bar.buttons.notifications.icon" = "${accent}";
-      "theme.notification.background" = "${background-alt}";
-      "theme.notification.actions.background" = "${accent}";
-      "theme.notification.actions.text" = "${foreground}";
-      "theme.notification.label" = "${accent}";
-      "theme.notification.border" = "${background-alt}";
-      "theme.notification.text" = "${foreground}";
-      "theme.notification.labelicon" = "${accent}";
-      "theme.osd.bar_color" = "${accent}";
-      "theme.osd.bar_overflow_color" = "${accent-alt}";
-      "theme.osd.icon" = "${background}";
-      "theme.osd.icon_container" = "${accent}";
-      "theme.osd.label" = "${accent}";
-      "theme.osd.bar_container" = "${background-alt}";
-      "theme.bar.menus.menu.media.background.color" = "${background-alt}";
-      "theme.bar.menus.menu.media.card.color" = "${background-alt}";
-      "theme.bar.menus.menu.media.card.tint" = 90;
-      "bar.customModules.updates.pollingInterval" = 1440000;
+      tear = true; # Screen Tearing
+      scalingPriority = "hyprland";
+      bar = {
+        customModules.updates.pollingInterval = 1440000;
+        launcher.icon = "";
+        workspaces = {
+          showAllActive = false;
+          workspaces = 1;
+          monitorSpecific = false;
+          hideUnoccupied = true;
+          showApplicationIcons = true;
+          showWsIcons = true;
+          ignored = "98";
+        };
+        windowtitle.label = false;
+        clock.format = "%a %d-%m-%Y  %H:%M:%S";
+        clock.showIcon = false;
+        volume.label = true;
+        bluetooth.label = false;
+        network.label = true;
+        media.show_active_only = true;
+      };
+      menus.clock.weather.enable = false;
+      wallpaper.enable = true;
+      theme = {
+        font.size = "0.85rem";
+        bar.outer_spacing = "1rem";
+        bar.dropdownGap = "2.3em";
+        bar.menus.monochrome = true;
+        bar.buttons.monochrome = true;
+        bar.buttons.workspaces.hover = "${accent-alt}";
+        bar.buttons.workspaces.active = "${accent}";
+        bar.buttons.workspaces.available = "${accent-alt}";
+        bar.menus.background = "${background}";
+        bar.menus.cards = "${background-alt}";
+        bar.menus.card_radius = "${toString rounding}px";
+        bar.menus.label = "${foreground}";
+        bar.menus.text = "${foreground}";
+        bar.menus.border.color = "${accent}";
+        bar.menus.border.radius = "${toString rounding}px";
+        bar.menus.popover.text = "${foreground}";
+        bar.menus.popover.background = "${background-alt}";
+        bar.menus.listitems.active = "${accent}";
+        bar.menus.icons.active = "${accent}";
+        bar.menus.switch.enabled = "${accent}";
+        bar.menus.buttons.default = "${accent}";
+        bar.menus.buttons.active = "${accent}";
+        notification.border_radius = "${toString rounding}px";
+        bar.menus.iconbuttons.active = "${accent}";
+        bar.menus.progressbar.foreground = "${accent}";
+        bar.menus.slider.primary = "${accent}";
+        bar.menus.tooltip.background = "${background-alt}";
+        bar.menus.tooltip.text = "${foreground}";
+        bar.menus.dropdownmenu.background = "${background-alt}";
+        bar.menus.dropdownmenu.text = "${foreground}";
+        bar.background = "${background}";
+        bar.buttons.text = "${foreground}";
+        bar.buttons.radius = "${toString rounding}px";
+        bar.buttons.background = "${background-alt}";
+        bar.buttons.icon = "${accent}";
+        bar.buttons.notifications.background = "${background-alt}";
+        bar.buttons.hover = "${background}";
+        bar.buttons.notifications.hover = "${background}";
+        bar.buttons.notifications.total = "${accent}";
+        bar.buttons.notifications.icon = "${accent}";
+        notification.background = "${background-alt}";
+        notification.actions.background = "${accent}";
+        notification.actions.text = "${foreground}";
+        notification.label = "${accent}";
+        notification.border = "${background-alt}";
+        notification.text = "${foreground}";
+        notification.labelicon = "${accent}";
+        osd.bar_color = "${accent}";
+        osd.bar_overflow_color = "${accent-alt}";
+        osd.icon = "${background}";
+        osd.icon_container = "${accent}";
+        osd.label = "${accent}";
+        osd.bar_container = "${background-alt}";
+        bar.menus.menu.media.background.color = "${background-alt}";
+        bar.menus.menu.media.card.color = "${background-alt}";
+        bar.menus.menu.media.card.tint = 90;
+      };
     };
   };
 }
