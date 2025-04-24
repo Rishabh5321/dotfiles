69f794672f969192d76907edbb70fdc40041afeb
Rishabh <40533251+Rishabh5321@users.noreply.github.com>
2025-04-24 11:41:43 +0530

    chore: auto lint and format (#1768)

--- flake.nix
+++ flake.nix
@@ -86,8 +86,8 @@
             home-manager.nixosModules.home-manager
             {
               nixpkgs.overlays = [
-                  inputs.hyprpanel.overlay
-                ];
+                inputs.hyprpanel.overlay
+              ];
             }
             # Home Manager configuration integrated into NixOS
             {
