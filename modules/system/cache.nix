837adb96315e4d5e437da56468f7737d6f6a2095
Rishabh5321 <40533251+Rishabh5321@users.noreply.github.com>
2025-04-25 12:39:07 +0530

    chore(build): add Hyprland cache to substituters and trusted keys in build configuration

--- modules/system/cache.nix
+++ modules/system/cache.nix
@@ -3,6 +3,7 @@
     substituters = [
       #"https://rishabh5321.cachix.org"
       "https://cache.nixos.org"
+      "https://hyprland.cachix.org"
       "https://nixpkgs-wayland.cachix.org"
       "https://cosmic.cachix.org/"
       "https://nix-config.cachix.org"
@@ -13,6 +14,7 @@
     trusted-substituters = [
       #"https://rishabh5321.cachix.org"
       "https://cache.nixos.org"
+      "https://hyprland.cachix.org"
       "https://nixpkgs-wayland.cachix.org"
       "https://cosmic.cachix.org/"
       "https://nix-config.cachix.org"
@@ -23,6 +25,7 @@
     trusted-public-keys = [
       #"rishabh5321.cachix.org-1:mxfBIH2XElE6ieFXXYBA9Ame4mVTbAf1TGR843siggk="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
       "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
       "nix-config.cachix.org-1:Vd6raEuldeIZpttVQfrUbLvXJHzzzkS0pezXCVVjDG4="
