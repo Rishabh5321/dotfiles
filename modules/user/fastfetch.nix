4510cac3b6716bc71cb212c23c47be84c9e79058
Rishabh <40533251+Rishabh5321@users.noreply.github.com>
2025-04-30 12:36:41 +0530

    chore: auto lint and format (#1880)

--- modules/user/fastfetch.nix
+++ modules/user/fastfetch.nix
@@ -1,115 +1,115 @@
 {
-    programs.fastfetch = {
-        enable = true;
+  programs.fastfetch = {
+    enable = true;
 
-        settings = {
-            /*logo = {
+    settings = {
+      /*logo = {
                 source = "~/nix/home-manager/modules/programs/fastfetch/pngs/hypr.png";
                 type = "kitty";
-    	        height = 19;
-    	        padding = {
-    		        top = 2;
-    	        };
+             	        height = 19;
+             	        padding = {
+              		        top = 2;
+             	        };
             };*/
 
-            display = {
-                separator = " ";
-            };
+      display = {
+        separator = " ";
+      };
 
-            modules = [
-                #"break"
-                #"break"
-                #"break"
-	            {
-                    type = "custom";
-                    format = "  {#90}  {#31}  {#33}  {#33}  {#34}  {#35}  {#36}  {#37} ";
-                }
-                "break"
-                {
-                    type = "custom";
-                    format = " {#36}   I use NixOs BTW";
-                }
-                {
-                    type = "os";
-                    key = "    OS";
-                    keyColor = "34";
-                }
-                {
-                    type = "kernel";
-                    key = "    Kernel";
-                    keyColor = "34";
-                }
-                {
-                    type = "packages";
-                    key = "    Packages";
-                    keyColor = "34";
-                }
-                {
-                    type = "display";
-                    key = "  󰍹  Display";
-                    keyColor = "34";
-                }
-                {
-                    type = "wm";
-                    key = "    WM";
-                    keyColor = "34";
-                }
-                {
-                    type = "terminal";
-                    key = "    Terminal";
-                    keyColor = "34";
-                }
-                {
-                    type = "cpu";
-                    format = "{1}";
-                    key = "    CPU";
-                    keyColor = "34";
-                }
-                {
-                    type = "memory";
-                    key = "    Memory";
-                    keyColor = "34";
-                }
-                {
-                    type = "cursor";
-                    key = "   ";
-                    keyColor = "34";
-                }
-                {
-                    type = "terminalfont";
-                    key = "   ";
-                    keyColor = "34";
-                }
-                {
-                    type = "uptime";
-                    key = "   ";
-                    keyColor = "34";
-                }
-                {
-                    type = "datetime";
-                    format = "{1}-{3}-{11}";
-                    key = "   ";
-                    keyColor = "34";
-                }
-                {
-                    type = "media";
-                    key = "  󰝚 ";
-                    keyColor = "34";
-                }
-                {
-                    type = "player";
-                    key = "   ";
-                    keyColor = "34";
-                }
-                "break"
-                {
-                    type = "custom";
-                    format = "  {#90}  {#31}  {#33}  {#33}  {#34}  {#35}  {#36}  {#37} ";
-                }
-                "break"
-                "break"
-                "break"
-            ];
-        };
+      modules = [
+        #"break"
+        #"break"
+        #"break"
+        {
+          type = "custom";
+          format = "  {#90}  {#31}  {#33}  {#33}  {#34}  {#35}  {#36}  {#37} ";
+        }
+        "break"
+        {
+          type = "custom";
+          format = " {#36}   I use NixOs BTW";
+        }
+        {
+          type = "os";
+          key = "    OS";
+          keyColor = "34";
+        }
+        {
+          type = "kernel";
+          key = "    Kernel";
+          keyColor = "34";
+        }
+        {
+          type = "packages";
+          key = "    Packages";
+          keyColor = "34";
+        }
+        {
+          type = "display";
+          key = "  󰍹  Display";
+          keyColor = "34";
+        }
+        {
+          type = "wm";
+          key = "    WM";
+          keyColor = "34";
+        }
+        {
+          type = "terminal";
+          key = "    Terminal";
+          keyColor = "34";
+        }
+        {
+          type = "cpu";
+          format = "{1}";
+          key = "    CPU";
+          keyColor = "34";
+        }
+        {
+          type = "memory";
+          key = "    Memory";
+          keyColor = "34";
+        }
+        {
+          type = "cursor";
+          key = "   ";
+          keyColor = "34";
+        }
+        {
+          type = "terminalfont";
+          key = "   ";
+          keyColor = "34";
+        }
+        {
+          type = "uptime";
+          key = "   ";
+          keyColor = "34";
+        }
+        {
+          type = "datetime";
+          format = "{1}-{3}-{11}";
+          key = "   ";
+          keyColor = "34";
+        }
+        {
+          type = "media";
+          key = "  󰝚 ";
+          keyColor = "34";
+        }
+        {
+          type = "player";
+          key = "   ";
+          keyColor = "34";
+        }
+        "break"
+        {
+          type = "custom";
+          format = "  {#90}  {#31}  {#33}  {#33}  {#34}  {#35}  {#36}  {#37} ";
+        }
+        "break"
+        "break"
+        "break"
+      ];
     };
+  };
 }
