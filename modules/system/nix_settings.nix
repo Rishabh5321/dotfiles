d3930898313e503c002346873e5ab68649659c3e
Rishabh5321 <40533251+Rishabh5321@users.noreply.github.com>
2025-05-13 07:21:52 +0530

    Set GRUB default to Windows and remove unused Cachix key
    - Configured GRUB to boot Windows by default for family use.
    - Removed unused "ezkea.cachix.org" substituter and public key.

--- modules/system/nix_settings.nix
+++ modules/system/nix_settings.nix
@@ -12,7 +12,6 @@
         "https://cosmic.cachix.org/"
         "https://nix-config.cachix.org"
         "https://nix-community.cachix.org"
-        "https://ezkea.cachix.org"
       ];
       trusted-substituters = [
         "https://rishabh5321.cachix.org"
@@ -21,7 +20,6 @@
         "https://cosmic.cachix.org/"
         "https://nix-config.cachix.org"
         "https://nix-community.cachix.org"
-        "https://ezkea.cachix.org"
       ];
       trusted-public-keys = [
         "rishabh5321.cachix.org-1:mxfBIH2XElE6ieFXXYBA9Ame4mVTbAf1TGR843siggk="
@@ -30,7 +28,6 @@
         "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
         "nix-config.cachix.org-1:Vd6raEuldeIZpttVQfrUbLvXJHzzzkS0pezXCVVjDG4="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="
       ];
       auto-optimise-store = true;
       experimental-features = [
