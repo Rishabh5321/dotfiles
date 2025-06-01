029ad42b9b1ba56a2690ff7741ff74a64c69ce59
Rishabh <40533251+Rishabh5321@users.noreply.github.com>
2025-06-01 07:28:06 +0530

    Change wallpaper from wall5.png to wall12.jpg

--- flake.nix
+++ flake.nix
@@ -74,7 +74,7 @@
     let
       system = "x86_64-linux";
       username = "rishabh";
-      wallpaper = "wall5.png";
+      wallpaper = "wall12.jpg";
       flakeDir = "/home/${username}/dotfiles";
 
       pkgs-stable = import nixpkgs-stable {
