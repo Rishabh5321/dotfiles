10e60bc758234dedaf7388331a2ebe7197ee8556
Rishabh <40533251+Rishabh5321@users.noreply.github.com>
2025-06-07 10:41:49 +0530

    Enable Tailscale routing and exit node features

--- hosts/server/tailscale.nix
+++ hosts/server/tailscale.nix
@@ -1,4 +1,16 @@
-_:
+{ config, lib, pkgs, username, ... }:
 {
-  services.tailscale.enable = true;
+  boot.kernel.sysctl = {
+    "fs.inotify.max_user_instances" = 1024;
+
+    # Need for tailscale
+    "net.ipv4.ip_forward" = 1;
+    "net.ipv6.conf.all.forwarding" = 1;
+  };
+
+  services.tailscale = {
+    enable = true;
+    useRoutingFeatures = "server";
+    extraUpFlags = "--advertise-exit-node";
+  };
 }
