19f3339948de202d138082cd48d11273a0343c98
Rishabh5321 <40533251+Rishabh5321@users.noreply.github.com>
2025-06-09 14:41:22 +0000

    chore: auto lint and format

--- modules/desktop/KDE/home/kde.nix
+++ modules/desktop/KDE/home/kde.nix
@@ -1,11 +1,11 @@
-{lib , ...}:
+{ lib, ... }:
 {
   stylix.targets.qt.platform = "qtct";
   # stylix.targets.kde.enable = true;
   # Qt configuration
   qt = {
-  enable = lib.mkForce false;
-  #platformTheme.name = "kde"; # Align with Stylix's supported platform
-  #style.name = "breeze";
+    enable = lib.mkForce false;
+    #platformTheme.name = "kde"; # Align with Stylix's supported platform
+    #style.name = "breeze";
   };
 }
