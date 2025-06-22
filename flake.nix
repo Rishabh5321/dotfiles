51ed3e87889bb9cf7046e35dc7bc806ebdc971bf
Rishabh <40533251+Rishabh5321@users.noreply.github.com>
2025-06-22 20:08:22 +0530

    Update wallpaper to wall53.jpg

--- flake.nix
+++ flake.nix
@@ -90,7 +90,7 @@
     let
       system = "x86_64-linux";
       username = "rishabh";
-      wallpaper = "wall23.jpg";
+      wallpaper = "wall53.jpg";
       flakeDir = "/home/${username}/dotfiles";
       wallpapers = "${wallpapers-repo}/src";
 
