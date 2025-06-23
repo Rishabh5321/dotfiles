88b0966067a79471ecaa04db2666af3caaefa331
Rishabh <40533251+Rishabh5321@users.noreply.github.com>
2025-06-23 18:02:47 +0530

    Replace systemd-resolved with dnscrypt-proxy2

--- modules/system/network/dns.nix
+++ modules/system/network/dns.nix
@@ -1,22 +1,49 @@
-_: {
+{ ... }:
+
+{
+  # Enable Encrypted DNS
   networking = {
-    # Disable resolvconf to avoid conflicts with systemd-resolved
-    resolvconf.enable = false;
+    nameservers = [ "127.0.0.1" "[::1]" ];
+    # If using dhcpcd:
+    dhcpcd.enable = false; # disable, because enabled by default
+    dhcpcd.extraConfig = "nohook resolv.conf";
+
+    # If using NetworkManager:
+    networkmanager.dns = "none";
+
+    # If using resolvconf:
+    resolvconf = {
+      enable = true; # FIXME remember to delete /etc/resolv.conf if you disable `resolvconf`
+      useLocalResolver = true;
+    };
+
+    # If using iwd:
+    wireless.iwd.settings.Network.NameResolvingService = "none";
   };
 
-  services.resolved = {
+  services.dnscrypt-proxy2 = {
     enable = true;
-    dnssec = "true"; # Enable DNSSEC validation
-    fallbackDns = [ "8.8.8.8" "8.8.4.4" ]; # Fallback to Google DNS
-    extraConfig = ''
-      DNS=1.1.1.1 1.0.0.1
-      DNSOverTLS=yes
-      DNSOverHTTPS=yes
-      DNSSEC=yes
-      FallbackDNS=8.8.8.8 8.8.4.4
-      # Use Cloudflare's DoH endpoints
-      DNSOverHTTPS=yes
-      DNSOverHTTPS=1.1.1.1#cloudflare-dns.com 1.0.0.1#cloudflare-dns.com
-    '';
+    settings = {
+      listen_addresses = [ "127.0.0.1:53" "[::1]:53" ];
+
+      ipv6_servers = true;
+      require_dnssec = true;
+
+      sources.public-resolvers = {
+        urls = [
+          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+        ];
+        cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
+        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+      };
+
+      # You can choose a specific set of servers from https://github.com/DNSCrypt/dnscrypt-resolvers/blob/master/v3/public-resolvers.md
+      server_names = [ "cloudflare" "cloudflare-ipv6" "cloudflare-security" "cloudflare-security-ipv6" "adguard-dns-doh" "mullvad-adblock-doh" "mullvad-doh" "nextdns" "nextdns-ipv6" "quad9-dnscrypt-ipv4-filter-pri" "google" "google-ipv6" "ibksturm" ];
+    };
+  };
+
+  systemd.services.dnscrypt-proxy2.serviceConfig = {
+    StateDirectory = "dnscrypt-proxy";
   };
 }
