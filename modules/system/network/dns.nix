049069cca379d0ec66b01788ac6f5e69051cf5c5
Rishabh <40533251+Rishabh5321@users.noreply.github.com>
2025-06-25 10:20:39 +0530

    Enable DNS caching and add more DNS providers

--- modules/system/network/dns.nix
+++ modules/system/network/dns.nix
@@ -2,17 +2,22 @@
   services.dnscrypt-proxy2 = {
     enable = true;
     settings = {
-      server_names = [ "cloudflare" ];
+      server_names = [ "cloudflare" "quad9-doh" "google" ];
+
       ipv6_servers = true;
       require_dnssec = true;
-
-      # Optional: use DoH or DoQ explicitly
       dnscrypt_servers = false;
       doh_servers = true;
       require_nolog = true;
       require_nofilter = true;
 
       listen_addresses = [ "127.0.0.1:53" ];
+
+      # DNS Cache Settings
+      cache = true;
+      cache_size = 4096; # Number of cached entries
+      cache_min_ttl = 600; # Minimum TTL in seconds (10 minutes)
+      cache_max_ttl = 86400; # Maximum TTL in seconds (1 day)
     };
   };
 
