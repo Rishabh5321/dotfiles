74cdc64eeb17c9ca3dd59723335ee54003c0e6e8
Rishabh <40533251+Rishabh5321@users.noreply.github.com>
2025-06-25 19:52:15 +0530

    Update wallpaper and disable DNS module

--- flake.nix
+++ flake.nix
@@ -90,7 +90,7 @@
     let
       system = "x86_64-linux";
       username = "rishabh";
-      wallpaper = "wall1.jpg";
+      wallpaper = "wall7.jpg";
       flakeDir = "/home/${username}/dotfiles";
       wallpapers = "${wallpapers-repo}/src";
 
