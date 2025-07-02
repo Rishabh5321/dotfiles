94bbaaec691e3c0bb20117413901be80e35841f2
Rishabh <40533251+Rishabh5321@users.noreply.github.com>
2025-07-02 09:20:09 +0530

    Update nixpkgs to nixos-unstable
    Use the full nixos-unstable channel.

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Core NixOS/Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     nur = {
