9f8ed8392185a67f6ebc87a01b212d89017e3dfd
Rishabh5321 <rishabh98818@outlook.com>
2025-07-11 09:50:10 +0530

    fix: use default package for gemini-cli
    The `gemini-cli-stable` package is not available anymore.
    Use the default package instead.

--- modules/system/packages/pkgs.nix
+++ modules/system/packages/pkgs.nix
@@ -172,7 +172,7 @@
 
     # ===== FLAKE INPUTS =====
     inputs.grayjay.packages.${pkgs.system}.grayjay # Video platform client
-    inputs.gemini-cli.packages.${pkgs.system}.gemini-cli-stable # Dev version with `nightly` tag
+    inputs.gemini-cli.packages.${pkgs.system}.default # Dev version with `nightly` tag
     inputs.zed-editor-flake.packages.${system}.zed-editor # Zed editor from flake
 
     # ===== FLAKE INPUTS (COMMENTED) =====
