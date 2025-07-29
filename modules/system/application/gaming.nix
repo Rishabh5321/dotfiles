2007da9b3063caa52ef1c9c607fe988e2fdb849e
Rishabh5321 <rishabh98818@outlook.com>
2025-07-29 19:23:27 +0530

    chore(gaming): remove wine-osu and wine-tkg
    These packages are commented out.

--- modules/system/application/gaming.nix
+++ modules/system/application/gaming.nix
@@ -6,8 +6,8 @@
 
   environment.systemPackages = [
     inputs.nix-gaming.packages.${pkgs.system}.wine-ge
-    inputs.nix-gaming.packages.${pkgs.system}.wine-osu
-    inputs.nix-gaming.packages.${pkgs.system}.wine-tkg
+    # inputs.nix-gaming.packages.${pkgs.system}.wine-osu
+    # inputs.nix-gaming.packages.${pkgs.system}.wine-tkg
   ];
 
   programs = {
