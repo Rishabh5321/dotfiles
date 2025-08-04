ead8b648e9df1e1c4f9e808bbc6e5c118db96882
Rishabh5321 <rishabh98818@outlook.com>
2025-08-04 18:05:07 +0530

    Add Hyprlock configuration with dynamic elements and styling
    Update to use Nix configuration format instead of raw config file Add
    dynamic date, time, uptime, and weather display Configure input field
    styling and background effects Remove commented-out sections and unused
    options

--- modules/desktop/Hyprland/home/core/hyprlock.nix
+++ modules/desktop/Hyprland/home/core/hyprlock.nix
@@ -1,4 +1,5 @@
 { config
+, lib
 , wallpaper
 , ...
 }:
@@ -10,151 +11,101 @@ in
 {
   programs.hyprlock = {
     enable = true;
-
-    sourceFirst = true;
+    settings = {
+      general = {
+        grace = 1;
+      };
+      background = {
+        path = lib.mkForce "~/Pictures/Wallpapers/${wallpaper}";
+        blur_size = 5;
+        blur_passes = 1;
+        noise = 0.0117;
+        contrast = 1.3;
+        brightness = 0.8;
+        vibrancy = 0.21;
+        vibrancy_darkness = 0.0;
+      };
+      "input-field" = {
+        size = "250, 50";
+        outline_thickness = 3;
+        dots_size = 0.33;
+        dots_spacing = 0.15;
+        dots_center = true;
+        outer_color = lib.mkForce color5;
+        inner_color = lib.mkForce color0;
+        font_color = lib.mkForce color14;
+        fade_on_empty = true;
+        placeholder_text = "<i>Password...</i>";
+        hide_input = false;
+        position = "0, 40";
+        halign = "center";
+        valign = "bottom";
+      };
+      "label" = lib.mkForce [
+        {
+          text = "cmd[update:18000000] echo \"<b> $(date +'%A, %-d %B %Y') </b>\"";
+          color = color14;
+          font_size = 34;
+          font_family = "JetBrains Mono Nerd Font Mono ExtraBold";
+          position = "0, -100";
+          halign = "center";
+          valign = "top";
+        }
+        {
+          text = "cmd[update:1000] echo -e \"$(date +'%I')\""; #AM/PM
+          color = "rgba(255, 185, 0, .6)";
+          font_size = 200;
+          font_family = "JetBrains Mono Nerd Font Mono ExtraBold";
+          position = "0, -200";
+          halign = "center";
+          valign = "top";
+        }
+        {
+          text = "cmd[update:1000] echo -e \"$(date +'%M')\"";
+          color = "rgba(255, 255, 255, .6)";
+          font_size = 200;
+          font_family = "JetBrains Mono Nerd Font Mono ExtraBold";
+          position = "0, -500";
+          halign = "center";
+          valign = "top";
+        }
+        {
+          text = "cmd[update:1000] echo -e \"$(date +'%S %p')\""; #AM/PM
+          color = color14;
+          font_size = 40;
+          font_family = "JetBrains Mono Nerd Font Mono ExtraBold";
+          position = "0, -500";
+          halign = "center";
+          valign = "top";
+        }
+        {
+          text = "   $USER";
+          color = color14;
+          font_size = 18;
+          font_family = "Inter Display Medium";
+          position = "0, 100";
+          halign = "center";
+          valign = "bottom";
+        }
+        {
+          text = "cmd[update:60000] echo \"<b> $(uptime -p || $Scripts/UptimeNixOS.sh) </b>\"";
+          color = color14;
+          font_size = 24;
+          font_family = "JetBrains Mono Nerd Font Mono ExtraBold";
+          position = "0, 0";
+          halign = "right";
+          valign = "bottom";
+        }
+        {
+          text = "cmd[update:3600000] [ -f ~/.cache/.weather_cache ] && cat  ~/.cache/.weather_cache";
+          color = color14;
+          font_size = 24;
+          font_family = "JetBrains Mono Nerd Font Mono ExtraBold";
+          position = "50, 0";
+          halign = "left";
+          valign = "bottom";
+        }
+      ];
+    };
   };
-  xdg.configFile."hypr/hyprlock.conf".text = ''
-    general {
-        grace = 1
-    }
-
-    background {
-        monitor =
-    	#path = screenshot   # screenshot of your desktop
-    	path = ~/Pictures/Wallpapers/${wallpaper}  # NOTE only png supported for now
-        #color = ${color0}
-
-        # all these options are taken from hyprland, see https://wiki.hyprland.org/Configuring/Variables/#blur for explanations
-        blur_size = 5
-        blur_passes = 1 # 0 disables blurring
-        noise = 0.0117
-        contrast = 1.3000 # Vibrant!!!
-        brightness = 0.8000
-        vibrancy = 0.2100
-        vibrancy_darkness = 0.0
-    }
-
-    #input-field {
-    #    monitor =
-    #    size = 250, 50
-    #    outline_thickness = 3
-    #    dots_size = 0.33 # Scale of input-field height, 0.2 - 0.8
-    #    dots_spacing = 0.15 # Scale of dots' absolute size, 0.0 - 1.0
-    #    dots_center = true
-    #    outer_color = ${color5}
-    #    inner_color = ${color0}
-    #    font_color = ${color14}
-    #    fade_on_empty = true
-    #    placeholder_text = <i>Password...</i> # Text rendered in the input box when it's empty.
-    #    hide_input = false
-
-    #    position = 0, 200
-    #    halign = center
-    #    valign = bottom
-    #}
-
-    # Date
-    label {
-        monitor =
-        text = cmd[update:18000000] echo "<b> "$(date +'%A, %-d %B %Y')" </b>"
-        color = ${color14}
-        font_size = 34
-        font_family = JetBrains Mono Nerd Font Mono ExtraBold
-        position = 0, -100
-        halign = center
-        valign = top
-    }
-
-    # Hour-Time
-    label {
-        monitor =
-    #     text = cmd[update:1000] echo -e "$(date +"%H")"
-        text = cmd[update:1000] echo -e "$(date +"%I")" #AM/PM
-        color = rgba(255, 185, 0, .6)
-        font_size = 200
-        font_family = JetBrains Mono Nerd Font Mono ExtraBold
-        position = 0, -200
-        halign = center
-        valign = top
-    }
-
-    # Minute-Time
-    label {
-        monitor =
-        text = cmd[update:1000] echo -e "$(date +"%M")"
-        color = rgba(255, 255, 255, .6)
-        font_size = 200
-        font_family = JetBrains Mono Nerd Font Mono ExtraBold
-        position = 0, -500
-        halign = center
-        valign = top
-    }
-
-    # Seconds-Time
-    label {
-        monitor =
-    #     text = cmd[update:1000] echo -e "$(date +"%S")"
-        text = cmd[update:1000] echo -e "$(date +"%S %p")" #AM/PM
-        color = ${color14}
-        font_size = 40
-        font_family = JetBrains Mono Nerd Font Mono ExtraBold
-        position = 0, -500
-        halign = center
-        valign = top
-    }
-
-    # User
-    label {
-        monitor =
-        text =    $USER
-        color = ${color14}
-        font_size = 18
-        font_family = Inter Display Medium
-
-        position = 0, 100
-        halign = center
-        valign = bottom
-    }
-
-    # uptime
-    label {
-        monitor =
-        text = cmd[update:60000] echo "<b> "$(uptime -p || $Scripts/UptimeNixOS.sh)" </b>"
-        color = ${color14}
-        font_size = 24
-        font_family = JetBrains Mono Nerd Font Mono ExtraBold
-        position = 0, 0
-        halign = right
-        valign = bottom
-    }
-
-    # weather edit the scripts for locations
-    # weather scripts are located in ~/.config/hypr/UserScripts Weather.sh and/or Weather.py
-    # see https://github.com/JaKooLit/Hyprland-Dots/wiki/TIPS#%EF%B8%8F-weather-app-related-for-waybar-and-hyprlock
-    label {
-        monitor =
-        text = cmd[update:3600000] [ -f ~/.cache/.weather_cache ] && cat  ~/.cache/.weather_cache
-        color = ${color14}
-        font_size = 24
-        font_family = JetBrains Mono Nerd Font Mono ExtraBold
-        position = 50, 0
-        halign = left
-        valign = bottom
-    }
-
-    # Put a picture of choice here. Default is the current wallpaper
-    # image {
-    #     monitor =
-    #     path = $HOME/.config/hypr/wallpaper_effects/.wallpaper_current
-    #     size = 230
-    #     rounding = -1
-    #     border_size = 2
-    #     border_color = ${color14}
-    #     rotate = 0
-    #     reload_time = -1
-    #     position = 0, 300
-    #     halign = center
-    #     valign = bottom
-    # }
-  '';
 }
