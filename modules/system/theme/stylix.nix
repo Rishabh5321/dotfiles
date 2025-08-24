56b753431b75e2566cf34566e009e679b013237a
Rishabh5321 <rishabh98818@outlook.com>
2025-08-24 14:18:41 +0530

    feat(stylix): update theme fonts and add icon theme
    Switched serif, sans-serif, and monospace fonts to Noto and Hack.
    Enabled Papirus icon theme and expanded opacity settings.
    Also enabled font package targets.

--- modules/system/theme/stylix.nix
+++ modules/system/theme/stylix.nix
@@ -7,28 +7,28 @@
     enable = true;
     image = "${wallpapers}/${wallpaper}";
     polarity = "dark";
-    opacity.terminal = 1.0;
 
     cursor = {
       package = pkgs.afterglow-cursors-recolored;
       name = "Afterglow-Recolored-Catppuccin-Flamingo";
       size = 24;
     };
+
     fonts = {
 
-      serif = {
-        package = pkgs.aleo-fonts;
-        name = "Aleo";
+      sansSerif = {
+        name = "Noto Sans";
+        package = pkgs.noto-fonts;
       };
 
-      sansSerif = {
-        package = pkgs.noto-fonts-cjk-sans;
-        name = "Noto Sans CJK JP";
+      serif = {
+        name = "Noto Serif";
+        package = pkgs.noto-fonts;
       };
 
       monospace = {
-        package = pkgs.nerd-fonts.jetbrains-mono;
-        name = "JetBrainsMono NFM";
+        package = pkgs.nerd-fonts.hack;
+        name = "Hack Nerd Font Mono";
       };
 
       emoji = {
@@ -42,6 +42,24 @@
         desktop = 11;
         popups = 12;
       };
+    };
+
+    iconTheme = {
+      enable = true;
+      package = pkgs.papirus-icon-theme;
+      dark = "Papirus-Dark";
+      light = "Papirus-Light";
+    };
+
+    opacity = {
+      applications = 1.0;
+      popups = 1.0;
+      terminal = 1.0;
+      desktop = 1.0;
+    };
+
+    targets = {
+      font-packages.enable = true;
     };
   };
 
