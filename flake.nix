d3a55575358c8bd95fc7e1723ec3dd886d0763fc
Rishabh5321 <rishabh98818@outlook.com>
2025-08-28 10:08:30 +0530

    Removing zen browser

--- flake.nix
+++ flake.nix
@@ -102,10 +102,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    zen-browser = {
-      url = "github:0xc000022070/zen-browser-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # zen-browser = {
+    #   url = "github:0xc000022070/zen-browser-flake";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
 
     ghostty = {
       url = "github:ghostty-org/ghostty";
