c1373a1c632ff7da3b8b5e4f035e3a533e0b85cb
Rishabh5321 <rishabh98818@outlook.com>
2025-09-04 20:52:30 +0530

    fix: update logind settings for lid switch handling in power-server.nix refactor: adjust inputs in gaming.nix for package management

--- modules/system/application/gaming.nix
+++ modules/system/application/gaming.nix
@@ -1,11 +1,11 @@
-{ pkgs, ... }: {
+{ pkgs, inputs, ... }: {
   nix.settings = {
     substituters = [ "https://nix-gaming.cachix.org" ];
     trusted-public-keys = [ "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=" ];
   };
 
   environment.systemPackages = [
-    # inputs.nix-gaming.packages.${pkgs.system}.wine-ge
+    inputs.nix-gaming.packages.${pkgs.system}.wine-ge
     # inputs.nix-gaming.packages.${pkgs.system}.wine-osu
     # inputs.nix-gaming.packages.${pkgs.system}.wine-tkg
   ];
