ce05e7b6b9144b6389cc0dad2a7450d5ac72af1a
Rishabh <40533251+Rishabh5321@users.noreply.github.com>
2025-09-04 17:30:27 +0530

    chore: auto format, lint and clean nix code (#4121)

--- modules/system/application/gaming.nix
+++ modules/system/application/gaming.nix
@@ -1,4 +1,4 @@
-{ pkgs, inputs, ... }: {
+{ pkgs, ... }: {
   nix.settings = {
     substituters = [ "https://nix-gaming.cachix.org" ];
     trusted-public-keys = [ "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=" ];
