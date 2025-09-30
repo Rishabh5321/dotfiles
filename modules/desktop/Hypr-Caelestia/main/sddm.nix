e4fdd5ecdaf84d06d849f4c8685121024124e3f2
Rishabh5321 <rishabh98818@outlook.com>
2025-09-30 09:16:18 +0530

    feat(sddm): configure sddm with astronaut theme
    Replaces the `sugarCandyNix` theme with `sddm-astronaut`.
    Configures the new theme with stylix-based colors and enables Wayland.

--- modules/desktop/Hypr-Caelestia/main/sddm.nix
+++ modules/desktop/Hypr-Caelestia/main/sddm.nix
@@ -1,42 +1,51 @@
-{ pkgs, config, lib, wallpaper, wallpapers, ... }: {
-  services.displayManager = {
-    enable = true;
-    sddm = {
-      enable = true;
-      wayland = {
-        enable = false;
-      };
-      # package = pkgs.kdePackages.sddm;
-      # extraPackages = with pkgs; [
-      #   kdePackages.qtsvg
-      #   kdePackages.qtmultimedia
-      #   kdePackages.qtvirtualkeyboard
-      # ];
-      # theme = "sddm-astronaut-theme";
-      # settings = {
-      #   Autologin = {
-      #     Session = "hyprland";
-      #     User = "rishabh";
-      #   };
-      # };
-      sugarCandyNix = {
-        enable = true;
-        settings = {
-          # General settings
-          AccentColor = "#${config.stylix.base16Scheme.base0B}";
-          Background = "${lib.cleanSource wallpapers}/${wallpaper}";
-          Font = "JetBrainsMono Nerd Font Mono";
-          # Form settings
-          HeaderText = "Welcome!";
-          FormPosition = "left";
-          HaveFormBackground = true;
-          PartialBlur = true;
-          HourFormat = "h:m:s ap";
-        };
-      };
-      autoNumlock = true;
+# SDDM is a display manager for X11 and Wayland
+{ pkgs
+, config
+, lib
+, wallpaper
+, wallpapers
+, ...
+}:
+let
+  foreground = config.stylix.base16Scheme.base00;
+  textColor = config.stylix.base16Scheme.base05;
+  sddm-astronaut = pkgs.sddm-astronaut.override {
+    embeddedTheme = "pixel_sakura";
+    themeConfig = {
+      FormPosition = "left";
+      Blur = "4.0";
+      Background = "${lib.cleanSource wallpapers}/${wallpaper}";
+      HeaderTextColor = "#${textColor}";
+      DateTextColor = "#${textColor}";
+      TimeTextColor = "#${textColor}";
+      LoginFieldTextColor = "#${textColor}";
+      PasswordFieldTextColor = "#${textColor}";
+      UserIconColor = "#${textColor}";
+      PasswordIconColor = "#${textColor}";
+      WarningColor = "#${textColor}";
+      LoginButtonBackgroundColor = "#${config.stylix.base16Scheme.base01}";
+      SystemButtonsIconsColor = "#${textColor}";
+      SessionButtonTextColor = "#${textColor}";
+      VirtualKeyboardButtonTextColor = "#${textColor}";
+      DropdownBackgroundColor = "#${config.stylix.base16Scheme.base01}";
+      HighlightBackgroundColor = "#${textColor}";
+      FormBackgroundColor = "#${config.stylix.base16Scheme.base01}";
     };
   };
+in
+{
+  services.displayManager = {
+    sddm = {
+      package = pkgs.kdePackages.sddm;
+      extraPackages = [ sddm-astronaut ];
+      enable = true;
+      wayland.enable = true;
+      theme = "sddm-astronaut-theme";
+    };
+  };
+
+  environment.systemPackages = [ sddm-astronaut ];
+
 
   security.pam.services.sddm.enableGnomeKeyring = true;
   security.pam.services.hyprlock.enableGnomeKeyring = true;
