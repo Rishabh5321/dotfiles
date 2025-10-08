cbd8c8e730a51f946909545745129a855d0b0abc
Rishabh5321 <rishabh98818@outlook.com>
2025-10-08 12:35:42 +0530

    chore(qemu): remove explicit ovmf config

--- modules/system/virtualization/virtualisation.nix
+++ modules/system/virtualization/virtualisation.nix
@@ -17,10 +17,6 @@
       enable = true;
       qemu = {
         swtpm.enable = true;
-        ovmf = {
-          enable = true;
-          packages = [ pkgs.OVMFFull.fd ];
-        };
       };
     };
     spiceUSBRedirection.enable = true;
