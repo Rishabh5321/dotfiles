886a995a9ed1e956842e44fc81ced355636ab0e3
Rishabh5321 <40533251+Rishabh5321@users.noreply.github.com>
2025-10-13 10:35:05 +0000

    chore: auto format, lint and clean nix code

--- hosts/server/hardware-configuration.nix
+++ hosts/server/hardware-configuration.nix
@@ -23,12 +23,14 @@
   ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/2c0c51e9-cb70-4ffa-9ea2-0ec4a9f51747";
+    {
+      device = "/dev/disk/by-uuid/2c0c51e9-cb70-4ffa-9ea2-0ec4a9f51747";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/33DC-F985";
+    {
+      device = "/dev/disk/by-uuid/33DC-F985";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
