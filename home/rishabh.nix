a70d9189256be4c5b43c785c088e08013e5f7bb2
Rishabh Singh <rishabh@redmi.lan>
2025-10-31 20:24:33 +0530

    feat(terminal): add ghostty configuration
    Configure Ghostty as the terminal emulator by including its specific module.

--- home/rishabh.nix
+++ home/rishabh.nix
@@ -8,7 +8,7 @@
     ./../modules/user/monitor
     ./../modules/user/shells
     ./../modules/user/shells/oh-my-posh.nix
-    ./../modules/user/terminal
+    ./../modules/user/terminal/ghostty.nix
     ./../modules/user/utilities/atuin.nix
     ./../modules/user/utilities/tealdeer.nix
     ./../modules/user/utilities/zoxide.nix
