97be3af61241d66f483e69d6ceeab9cf13cd51fe
rishabh5321 <rishabh98818@outlook.com>
2025-11-22 07:29:02 +0530

    feat(sddm): enable wayland and astronaut theme
    Enable Wayland support for SDDM.
    Configure `sddm-astronaut-theme` and add `sddm-astronaut` to system packages.
    Remove `sugarCandyNix` theme configuration.

--- modules/desktop/Hyprland/main/sddm.nix
+++ modules/desktop/Hyprland/main/sddm.nix
@@ -1,45 +1,49 @@
-{ pkgs, config, lib, wallpaper, wallpapers, ... }: {
+{ pkgs, ... }: {
   services.displayManager = {
     enable = true;
     sddm = {
       enable = true;
       wayland = {
-        enable = false;
+        enable = true;
       };
-      # package = pkgs.kdePackages.sddm;
-      # extraPackages = with pkgs; [
-      #   kdePackages.qtsvg
-      #   kdePackages.qtmultimedia
-      #   kdePackages.qtvirtualkeyboard
-      # ];
-      # theme = "sddm-astronaut-theme";
+      package = pkgs.kdePackages.sddm;
+      extraPackages = with pkgs; [
+        kdePackages.qtsvg
+        kdePackages.qtmultimedia
+        kdePackages.qtvirtualkeyboard
+      ];
+      theme = "sddm-astronaut-theme";
       # settings = {
       #   Autologin = {
       #     Session = "hyprland";
       #     User = "rishabh";
       #   };
       # };
-      sugarCandyNix = {
-        enable = true;
-        settings = {
-          # General settings
-          AccentColor = "#${config.stylix.base16Scheme.base0B}";
-          Background = "${lib.cleanSource wallpapers}/${wallpaper}";
-          Font = "JetBrainsMono Nerd Font Mono";
-          # Form settings
-          HeaderText = "Welcome!";
-          FormPosition = "left";
-          HaveFormBackground = true;
-          PartialBlur = true;
-          HourFormat = "h:m:s ap";
-        };
-      };
-      autoNumlock = true;
+      #   sugarCandyNix = {
+      #     enable = true;
+      #     settings = {
+      #       # General settings
+      #       AccentColor = "#${config.stylix.base16Scheme.base0B}";
+      #       Background = "${lib.cleanSource wallpapers}/${wallpaper}";
+      #       Font = "JetBrainsMono Nerd Font Mono";
+      #       # Form settings
+      #       HeaderText = "Welcome!";
+      #       FormPosition = "left";
+      #       HaveFormBackground = true;
+      #       PartialBlur = true;
+      #       HourFormat = "h:m:s ap";
+      #     };
+      #   };
+      #   autoNumlock = true;
     };
   };
 
   security.pam.services.sddm.enableGnomeKeyring = true;
   security.pam.services.hyprlock.enableGnomeKeyring = true;
+
+  environment.systemPackages = with pkgs; [
+    sddm-astronaut
+  ];
 
   services.xserver.displayManager.setupCommands = ''
     ${pkgs.xorg.xset}/bin/xset s off
