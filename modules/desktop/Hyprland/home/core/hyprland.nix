98f9f07ab35209d22d98226bd88b4a974da8166d
rishabh5321 <rishabh98818@outlook.com>
2026-01-17 12:09:25 +0530

    refactor(hyprland): remove commented out xwayland config
    Removes commented out settings for xwayland and inherit_fullscreen,
    as these are no longer needed or are handled elsewhere.

--- modules/desktop/Hyprland/home/core/hyprland.nix
+++ modules/desktop/Hyprland/home/core/hyprland.nix
@@ -15,9 +15,9 @@ with lib; {
       enableXdgAutostart = true;
       variables = [ "--all" ];
     };
-    settings = {
-      xwayland.enable = true;
-    };
+    # settings = {
+    #   xwayland.enable = true;
+    # };
 
     extraConfig =
       let
@@ -211,7 +211,7 @@ with lib; {
         # new_on_top = false
         # no_gaps_when_only = 0
         orientation = left
-        inherit_fullscreen = true
+        # inherit_fullscreen = true
         # always_center_master = false
         smart_resizing = true
         drop_at_cursor = true
