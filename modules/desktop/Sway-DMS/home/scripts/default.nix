014e94df8b8eb308b7b3f91ce44da70e4b2d0575
rishabh5321 <rishabh98818@outlook.com>
2026-01-31 12:40:30 +0530

    refactor(sway-dms): comment out unused scripts
    Comment out several scripts that are no longer in use to clean up the configuration.

--- modules/desktop/Sway-DMS/home/scripts/default.nix
+++ modules/desktop/Sway-DMS/home/scripts/default.nix
@@ -1,13 +1,13 @@
 { pkgs, username, host }:
 [
-  (import ./rofi-launcher.nix { inherit pkgs; })
-  (import ./emopicker9000.nix { inherit pkgs; })
-  (import ./task-waybar.nix { inherit pkgs; })
-  (import ./wallsetter.nix { inherit pkgs; inherit username; })
-  (import ./web-search.nix { inherit pkgs; })
+  # (import ./rofi-launcher.nix { inherit pkgs; })
+  # (import ./emopicker9000.nix { inherit pkgs; })
+  # (import ./task-waybar.nix { inherit pkgs; })
+  # (import ./wallsetter.nix { inherit pkgs; inherit username; })
+  # (import ./web-search.nix { inherit pkgs; })
   (import ./screenshootin.nix { inherit pkgs; })
-  (import ./list-sway-bindings.nix { inherit pkgs; inherit host; })
-  (import ./wall-selector.nix { inherit pkgs; })
+  # (import ./list-sway-bindings.nix { inherit pkgs; inherit host; })
+  # (import ./wall-selector.nix { inherit pkgs; })
   pkgs.wayland-protocols
   pkgs.wayland-utils
 ]
