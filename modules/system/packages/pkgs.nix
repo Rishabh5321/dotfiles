6175c806a52b59a73ad057753d9876aaef167589
rishabh5321 <rishabh98818@outlook.com>
2026-03-09 20:46:38 +0530

    refactor(packages): update custom app sourcing
    Move hydralauncher from standard pkgs to custom inputs.
    Add mangayomi, shonenx, and sorayomi to the custom package list.

--- modules/system/packages/pkgs.nix
+++ modules/system/packages/pkgs.nix
@@ -116,7 +116,7 @@
       pysolfc # Solitaire Game
       gamescope # Gaming compositor
       heroic # Epic Games launcher
-      hydralauncher
+      # hydralauncher
       # lutris # Gaming platform manager
       mangohud # Gaming overlay
       steam # Steam gaming platform
@@ -203,10 +203,14 @@
       inputs.custom-packages.packages.${pkgs.stdenv.hostPlatform.system}.anymex
       inputs.custom-packages.packages.${pkgs.stdenv.hostPlatform.system}.better-control # Device control
       inputs.custom-packages.packages.${pkgs.stdenv.hostPlatform.system}.fladder
+      inputs.custom-packages.packages.${pkgs.stdenv.hostPlatform.system}.hydralauncher
+      inputs.custom-packages.packages.${pkgs.stdenv.hostPlatform.system}.mangayomi
       inputs.custom-packages.packages.${pkgs.stdenv.hostPlatform.system}.grayjay
       inputs.custom-packages.packages.${pkgs.stdenv.hostPlatform.system}.playtorrio
       inputs.custom-packages.packages.${pkgs.stdenv.hostPlatform.system}.stremio
+      inputs.custom-packages.packages.${pkgs.stdenv.hostPlatform.system}.shonenx
       inputs.custom-packages.packages.${pkgs.stdenv.hostPlatform.system}.surge
+      inputs.custom-packages.packages.${pkgs.stdenv.hostPlatform.system}.sorayomi
       inputs.custom-packages.packages.${pkgs.stdenv.hostPlatform.system}.zed-editor
     ];
 
