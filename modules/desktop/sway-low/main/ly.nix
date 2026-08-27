cce5b059b2fcd9e8e293ef596678bb074cdbc838
rishabh5321 <rishabh98818@outlook.com>
2026-08-27 18:07:46 +0530

    Update Ly display manager configuration
    Switch to dur_file animation and enable session saving. Add a custom
    animation file sourced from the community repository.

--- modules/desktop/sway-low/main/ly.nix
+++ modules/desktop/sway-low/main/ly.nix
@@ -1,21 +1,26 @@
-_:
-{
+{pkgs,...}: {
   services.displayManager.ly = {
     enable = true;
     settings = {
-      auth_fails = 3;
-      hide_version_string = true;
-      hide_key_hints = true;
-      clear_password = true;
-      bg = "0x00555555";
-      fg = "0x00ffffff";
-      border_fg = "0x00ffffff";
-      error_bg = "0x00555555";
-      animation = "colormix";
-      colormix_col1 = "0x00aaaaaa";
-      colormix_col2 = "0x00555555";
-      colormix_col3 = "0x00000000";
+      animation = "dur_file";
+      bigclock = "en";
+      full_color = true;
+      save = true;
+      load = true;
+      # auto_login_session = "Niri";
+      # auto_login_user = config.identity.user;
+      default_login = "password";
+      dur_file_path = "${
+        pkgs.fetchFromGitea {
+          domain = "codeberg.org";
+          owner = "fairyglade";
+          repo = "ly-community";
+          rev = "2f22cfaf7d17598c8f60f562d56e16d74b6c99ab";
+          hash = "sha256-BQhlvWmEkXNpbUgtGBzbHjdQwRa2jxHhBBNu8sVzIDQ=";
+        }
+      }/animations/dur/blackhole-smooth-240x67.dur";
     };
   };
+
   boot.kernelParams = [ "consoleblank=60" ];
 }
