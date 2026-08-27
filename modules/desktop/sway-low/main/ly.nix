fe9fc75355175806da8c3dda38576fadbdc7e131
rishabh5321 <rishabh98818@outlook.com>
2026-08-27 17:25:11 +0530

    Configure Ly display manager settings

--- modules/desktop/sway-low/main/ly.nix
+++ modules/desktop/sway-low/main/ly.nix
@@ -1,5 +1,21 @@
 _:
 {
-  services.displayManager.ly.enable = true;
+  services.displayManager.ly = {
+    enable = true;
+    settings = {
+      auth_fails = 3;
+      hide_version_string = true;
+      hide_key_hints = true;
+      clear_password = true;
+      bg = "0x00555555";
+      fg = "0x00ffffff";
+      border_fg = "0x00ffffff";
+      error_bg = "0x00555555";
+      animation = "colormix";
+      colormix_col1 = "0x00aaaaaa";
+      colormix_col2 = "0x00555555";
+      colormix_col3 = "0x00000000";
+    };
+  };
   boot.kernelParams = [ "consoleblank=60" ];
 }
